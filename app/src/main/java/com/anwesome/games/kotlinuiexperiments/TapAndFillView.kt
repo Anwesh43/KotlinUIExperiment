2da2e9b9659c13be365a4f76a9c0a6d470bd6e35
Anwesh43 <anweshthecool0@gmail.com>
2017-07-04 15:43:12 +0530

    adding growing ball in TapAndFillView

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/TapAndFillView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/TapAndFillView.kt
@@ -2,6 +2,7 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
@@ -12,7 +13,6 @@ import android.view.View
 class TapAndFillView(ctx:Context):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
-
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
@@ -21,5 +21,29 @@ class TapAndFillView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class GrowindBall(var x:Float,var y:Float) {
+        var scale:Float = 0.0f
+        var mode:Int = 0
+        fun draw(canvas:Canvas,paint:Paint,r:Float) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.scale(scale,scale)
+            paint.color = Color.YELLOW
+            canvas.drawCircle(0.0f,0.0f,r,paint)
+            canvas.restore()
+        }
+        fun stopped(h:Float):Boolean = y>=h
+        fun update() {
+            if(scale >= 1) {
+                y+=20.0f
+            }
+            else {
+                scale += 0.2f
+                if(scale > 1) {
+                    scale = 1.0f
+                }
+            }
+        }
     }
 }
