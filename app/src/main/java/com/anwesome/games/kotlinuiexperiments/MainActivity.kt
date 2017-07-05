8d62ccafcf67b999c17f0aaefe66acc830b3ae46
Anwesh43 <anweshthecool0@gmail.com>
2017-07-05 14:07:01 +0530

    using ColorPageView in app

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/MainActivity.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/MainActivity.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import CircleCreatorView
+import android.graphics.Color
 import android.view.WindowManager
 import android.widget.Toast
 
@@ -12,11 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        var view:CircleCreatorView = CircleCreatorView(this)
 //        val completionListener = CompletionListener(this)
-        var view:TapAndFillView = TapAndFillView(this)
+        var view:ColorPageView = ColorPageView(this)
         //view.onCompletionListener = completionListener
         supportActionBar?.hide()
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        view.onBallOutOfBoundListener = BallOutOfBoundListener(this)
+        view.addColor(Color.parseColor("#E65100"))
+        view.addColor(Color.parseColor("#00ACC1"))
+        view.addColor(Color.parseColor("#f44336"))
+        view.addColor(Color.parseColor("#C2185B"))
         setContentView(view)
     }
 }
