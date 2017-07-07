38ee66afc96b3aa968fab27f32090304a0d62804
Anwesh43 <anweshthecool0@gmail.com>
2017-07-07 22:44:15 +0530

    adding DrawingController

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/SidewiseBallMover.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/SidewiseBallMover.kt
@@ -10,27 +10,48 @@ import android.view.View
  * Created by anweshmishra on 07/07/17.
  */
 class SidewiseBallMoverView(ctx:Context):View(ctx) {
+    var renderer:Renderer = Renderer()
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
     }
     class Renderer {
         var time = 0
-        fun render(canvas:Canvas,paint:Paint) {
+        var v:SidewiseBallMoverView?=null
+        var drawingController:DrawingController? = null
+        fun render(canvas:Canvas,paint:Paint,v:SidewiseBallMoverView) {
             if(time == 0) {
-                
+                drawingController = DrawingController(canvas.width,canvas.height,v)
             }
+            drawingController?.render(canvas,paint)
             time++
         }
         fun handleTap(x:Float,y:Float) {
+            drawingController?.handleTap(x,y)
+        }
+    }
+    class DrawingController {
+        var w:Int = 0
+        var h:Int = 0
+        var animated:Boolean = true
+        var v:SidewiseBallMoverView?=null
+        constructor(w:Int,h:Int,v:SidewiseBallMoverView) {
+            this.v = v
+            this.w = w
+            this.h = h
+        }
+        fun render(canvas:Canvas,paint:Paint) {
 
+        }
+        fun handleTap(x:Float,y:Float) {
         }
     }
 }
