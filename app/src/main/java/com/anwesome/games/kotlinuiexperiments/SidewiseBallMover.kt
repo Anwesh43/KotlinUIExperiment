3d4df79e9606df600edfb6a51e7b22df0da560bb
Anwesh43 <anweshthecool0@gmail.com>
2017-07-07 22:36:08 +0530

    adding renderer class

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/SidewiseBallMover.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/SidewiseBallMover.kt
@@ -2,6 +2,7 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
 
@@ -19,5 +20,17 @@ class SidewiseBallMoverView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    class Renderer {
+        var time = 0
+        fun render(canvas:Canvas,paint:Paint) {
+            if(time == 0) {
+                
+            }
+            time++
+        }
+        fun handleTap(x:Float,y:Float) {
+
+        }
     }
 }
