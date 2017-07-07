dc2c84455a4561a204263abd07c027ab99f46b98
Anwesh43 <anweshthecool0@gmail.com>
2017-07-07 23:01:23 +0530

    adding logic for DrawingController

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/SidewiseBallMover.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/SidewiseBallMover.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
+import java.util.concurrent.ConcurrentLinkedQueue
 
 /**
  * Created by anweshmishra on 07/07/17.
@@ -43,15 +44,50 @@ class SidewiseBallMoverView(ctx:Context):View(ctx) {
         var h:Int = 0
         var animated:Boolean = true
         var v:SidewiseBallMoverView?=null
+        var sideWiseBalls:ConcurrentLinkedQueue<SideWiseBall> = ConcurrentLinkedQueue()
+        var movingBalls:ConcurrentLinkedQueue<SideWiseBall> = ConcurrentLinkedQueue()
         constructor(w:Int,h:Int,v:SidewiseBallMoverView) {
             this.v = v
             this.w = w
             this.h = h
+            this.initBalls()
+        }
+        fun initBalls() {
+            val n = 8
+            val r:Float = (h/(2*n+1)).toFloat()
+            var y = 3*r
+            for(i in 1..n) {
+                sideWiseBalls.add(SideWiseBall((w/2).toFloat(),y,r))
+                y+=2*r
+            }
         }
         fun render(canvas:Canvas,paint:Paint) {
+            sideWiseBalls.forEach{ ball ->
+                ball.draw(canvas,paint)
+            }
+            if(animated) {
+                movingBalls.forEach {
+                    it.draw(canvas,paint)
+                }
+                try {
+                    Thread.sleep(75)
+                    v?.invalidate()
+                }
+                catch(ex:Exception) {
 
+                }
+            }
         }
         fun handleTap(x:Float,y:Float) {
+            sideWiseBalls.forEach { ball ->
+                if(ball.handleTap(x,y)) {
+                    movingBalls.add(ball)
+                    if(!animated) {
+                        animated = true
+                        v?.postInvalidate()
+                    }
+                }
+            }
         }
     }
     data class SideWiseBall(var x:Float,var y:Float,var r:Float) {
