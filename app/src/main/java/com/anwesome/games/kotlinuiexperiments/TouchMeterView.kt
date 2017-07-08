3d6c56242429f4ad1c7a4c5dc7bcc5e75bca8b39
Anwesh43 <anweshthecool0@gmail.com>
2017-07-08 13:47:47 +0530

    handling tap in TMVRenderer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/TouchMeterView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/TouchMeterView.kt
@@ -10,23 +10,37 @@ import android.view.View
  * Created by anweshmishra on 08/07/17.
  */
 class TouchMeterView(ctx:Context):View(ctx){
+    var renderer = TMVRenderer()
     val  paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
+        renderer.handleTap(event.action)
         return true
     }
     class TMVRenderer {
         var time = 0
+        var down = false
         fun render(canvas:Canvas,paint:Paint) {
             if(time == 0) {
 
             }
             time++
         }
-        fun handleTap(x:Float,y:Float) {
+        fun handleTap(action:Int) {
+            when(action) {
+                MotionEvent.ACTION_DOWN -> {
+                    if(!down) {
 
+                    }
+                }
+                MotionEvent.ACTION_UP -> {
+                    if(down) {
+
+                    }
+                }
+            }
         }
     }
 }
