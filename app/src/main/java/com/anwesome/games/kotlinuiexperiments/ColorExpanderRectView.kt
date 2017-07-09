bb294ce7ee8ecb26ca1d9a41af884a63e3f7e470
Anwesh43 <anweshthecool0@gmail.com>
2017-07-09 12:41:50 +0530

    using renderer in ColorExpanderRectView

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorExpanderRectView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorExpanderRectView.kt
@@ -13,10 +13,16 @@ import android.view.View
  */
 class ColorExpanderRectView(ctx:Context):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = CERVRenderer()
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                renderer.handleTap(event.x,event.y)
+            }
+        }
         return true
     }
     class CERVRenderer {
