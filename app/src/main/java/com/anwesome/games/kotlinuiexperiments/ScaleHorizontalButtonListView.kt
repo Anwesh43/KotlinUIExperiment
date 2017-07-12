d3a540fbda8ed63d46c38ac43c2df60204247f55
Anwesh43 <anweshthecool0@gmail.com>
2017-07-12 17:20:15 +0530

    Adding ScaleHorizontalButtonListView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ScaleHorizontalButtonListView.kt
@@ -0,0 +1,21 @@
+package com.anwesome.games.kotlinuiexperiments
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by anweshmishra on 12/07/17.
+ */
+
+class ScaleHorizontalButtonListView(ctx:Context):View(ctx) {
+    val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        return true
+    }
+}
