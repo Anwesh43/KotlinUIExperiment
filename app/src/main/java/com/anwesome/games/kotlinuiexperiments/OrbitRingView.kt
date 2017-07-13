2dfaee9dceb2a6abb93b6d3e6a217874c22bfd5b
Anwesh43 <anweshthecool0@gmail.com>
2017-07-13 13:39:52 +0530

    add OrbitRing

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/OrbitRingView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/OrbitRingView.kt
@@ -2,7 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -41,6 +43,46 @@ class OrbitRingView(ctx:Context):View(ctx) {
         }
         fun handleTap(x:Float,y:Float):Boolean {
             return true
+        }
+    }
+    data class OrbitRing(var x:Float,var y:Float,var size:Float) {
+        var scale = 0.0f
+        var dir = 0
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(360*scale)
+            paint.style = Paint.Style.STROKE
+            paint.color = Color.GRAY
+            paint.strokeWidth = size/60
+            canvas.drawCircle(size/2,size/2,size/2,paint)
+            paint.color = Color.BLUE
+            canvas.drawArc(RectF(-size/2,size/2,size/2,size/2),-90.0f,360*scale,false,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawCircle(0.0f,-size/2,size/15,paint)
+            canvas.restore()
+        }
+        fun update() {
+            scale += dir * 0.2f
+            if(scale > 1) {
+                scale = 1.0f
+                dir = 0
+            }
+            if(scale < 0) {
+                scale = 0.0f
+                dir = 0
+            }
+        }
+        fun stopped():Boolean {
+            return dir == 0
+        }
+        fun startUpdate() {
+            if(scale <= 0.0f) {
+                dir = 1
+            }
+            if(scale >= 1.0f) {
+                dir = -1
+            }
         }
     }
 }
