92779f98d7e817ee30c89de26862d4dfde4a2c82
Anwesh43 <anweshthecool0@gmail.com>
2017-07-15 09:50:17 +0530

    adding StateHandler

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DotAndLineView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DotAndLineView.kt
@@ -71,4 +71,27 @@ class DotAndLineView(ctx:Context,var n:Int):View(ctx) {
             }
         }
     }
+    class StateHandler {
+        var scale = 0.0f
+        var dir = 0
+        fun update() {
+            scale += dir*0.2f
+            if(scale > 1) {
+                scale = 1.0f
+                dir = 0
+            }
+            if(scale < 0) {
+                scale = 0.0f
+                dir = 0
+            }
+        }
+        fun startUpdating() {
+            dir = when(scale) {
+                0.0f -> 1
+                1.0f->-1
+                else -> dir
+            }
+        }
+        fun stopped():Boolean = dir == 0
+    }
 }
