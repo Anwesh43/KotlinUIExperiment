6c224cdab61ef5c1fd86337af5968fc0e8c620d1
Anwesh43 <anweshthecool0@gmail.com>
2017-07-15 09:41:48 +0530

    adding DotLine class

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DotAndLineView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DotAndLineView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -11,10 +12,30 @@ import android.view.View
  */
 class DotAndLineView(ctx:Context,var n:Int):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
 
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         return true
+    }
+    data class DotLine(var r:Float,var n:Int) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            if(n > 0) {
+                var deg = 360.0f/n
+                for (i in 0..n) {
+                    canvas.save()
+                    canvas.rotate(deg*i)
+                    paint.style = Paint.Style.STROKE
+                    canvas.drawCircle(0.0f,0.0f,r/10,paint)
+                    paint.style = Paint.Style.FILL
+                    canvas.drawArc(RectF(-r/10,-r/10,r/10,r/10),0.0f,360*scale,true,paint)
+                    var xFact = r*Math.cos(deg.toDouble()/2).toFloat()*scale
+                    var yFact = r*Math.sin(deg.toDouble()/2).toFloat()
+                    canvas.drawLine(-xFact,yFact,xFact,yFact,paint)
+                    canvas.restore()
+                }
+            }
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=-r/10 && x<=r/10 && y>=-r/10 && y<=r/10
     }
 }
