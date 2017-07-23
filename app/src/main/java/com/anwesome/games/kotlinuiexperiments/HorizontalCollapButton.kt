447412165aa3348adf0a8806eae72a21bdd69a76
Anwesh43 <anweshthecool0@gmail.com>
2017-07-23 10:47:12 +0530

    using Renderer in View

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/HorizontalCollapButton.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/HorizontalCollapButton.kt
@@ -10,13 +10,14 @@ import android.view.View
  */
 class HorizontalCollapButtonView(ctx:Context):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer:HCBRenderer = HCBRenderer()
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint,this)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
