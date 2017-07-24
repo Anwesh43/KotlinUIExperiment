322b2f5de63210e2ab5a0116a2d2cde643794036
Anwesh43 <anweshthecool0@gmail.com>
2017-07-24 09:27:51 +0530

    adding PSVRenderer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieSwitchList.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieSwitchList.kt
@@ -17,9 +17,21 @@ class PieSwitchListView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    class PSVRenderer  {
+        var time = 0
+        fun render(canvas:Canvas,paint: Paint,v:PieSwitchListView) {
+            if(time == 0) {
+
+            }
+            time++
+        }
+        fun handleTap(x:Float,y:Float) {
+
+        }
     }
 }
