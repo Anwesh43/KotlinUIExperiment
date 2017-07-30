42667724a4591137bcd8eaf1b7e33d9e51c74362
Anwesh43 <anweshthecool0@gmail.com>
2017-07-30 18:31:08 +0200

    adding scaleupindicator

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ScaleUpColorFilterImageView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ScaleUpColorFilterImageView.kt
@@ -39,4 +39,31 @@ class ScaleUpColorFilterImageView(ctx:Context,var color:Int=Color.CYAN,var bitma
             canvas.restore()
         }
     }
+    data class ScaleUpIndicator(var x:Float,var y:Float,var size:Float,var w:Float,var h:Float,var deg:Float = 0.0f) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(deg+45)
+            paint.style = Paint.Style.STROKE
+            canvas.restore()
+        }
+        private fun drawDirectionIndicator(canvas:Canvas,paint:Paint) {
+            paint.color = Color.WHITE
+            paint.strokeWidth = size/20
+            paint.strokeCap = Paint.Cap.ROUND
+            canvas.drawLine(0.0f,-size/3,0.0f,size/3,paint)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.scale(1.0f,2*i-1.0f)
+                canvas.drawLine(0.0f,-size/3,size/6,-size/6,paint)
+                canvas.restore()
+            }
+        }
+        fun update(scale:Float) {
+            deg = 180.0f*scale
+            x = w*scale
+            y = h*(1-scale)
+        }
+        fun handleTap(x:Float,y:Float):Boolean =  x>=this.x-size/2 && x<=this.x+size/2 && y>=this.y-size/2 && y<=this.y+size/2
+    }
 }
