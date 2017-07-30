6996271c25688ac5a557255b64399779b9a506f4
Anwesh43 <anweshthecool0@gmail.com>
2017-07-30 09:38:35 +0200

    adding ScaleUpColorFilterImageView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ScaleUpColorFilterImageView.kt
@@ -0,0 +1,26 @@
+package com.anwesome.games.kotlinuiexperiments
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by anweshmishra on 30/07/17.
+ */
+class ScaleUpColorFilterImageView(ctx:Context,var color:Int=Color.CYAN):View(ctx) {
+    val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas: Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
