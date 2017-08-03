5cb38cd413320687affe75c33b8e4932653e9a97
Anwesh43 <anweshthecool0@gmail.com>
2017-08-03 11:05:28 +0530

    adding BarButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/BarButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/BarButtonView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -22,5 +23,20 @@ class BarButtonView(ctx:Context,var n:Int = 5):View(ctx) {
             }
         }
         return true
+    }
+    data class BarButton(var x:Float,var y:Float,var r:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            canvas.save()
+            canvas.translate(x,y)
+            paint.style = Paint.Style.STROKE
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.strokeWidth = r/40
+            canvas.drawCircle(0.0f,0.0f,r,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-r,-r,r,r),0.0f,360.0f*scale,true,paint)
+            canvas.drawLine(0.0f,-h,0.0f,0.0f,paint)
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
     }
 }
