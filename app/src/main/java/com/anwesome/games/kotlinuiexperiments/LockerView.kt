10a850abe32de2405939e62075c9c8ddcc794592
Anwesh43 <anweshthecool0@gmail.com>
2017-08-04 12:57:39 +0530

    creating LockerView using static method

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/LockerView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/LockerView.kt
@@ -1,5 +1,6 @@
 package com.anwesome.games.kotlinuiexperiments
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -7,6 +8,7 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by anweshmishra on 04/08/17.
@@ -111,5 +113,12 @@ class LockerView(ctx:Context):View(ctx) {
             }
         }
         fun stopped():Boolean = dir == 0
+    }
+    companion object {
+        fun create(activity: Activity) {
+            var view = LockerView(activity)
+            var size = DimensionsUtil.getDimension(activity)
+            activity.addContentView(view,ViewGroup.LayoutParams(size.x/3,size.x/3))
+        }
     }
 }
