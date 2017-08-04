6f998456957938bdedaf6146f19f57aea80a3d9e
Anwesh43 <anweshthecool0@gmail.com>
2017-08-04 11:21:50 +0530

    adding LockerView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/LockerView.kt
@@ -0,0 +1,25 @@
+package com.anwesome.games.kotlinuiexperiments
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by anweshmishra on 04/08/17.
+ */
+class LockerView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
