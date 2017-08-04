37b5da13f333dfbfcbbd8a8324c7ea11ac600c13
Anwesh43 <anweshthecool0@gmail.com>
2017-08-04 11:41:06 +0530

    adding Locker class

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/LockerView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/LockerView.kt
@@ -2,7 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -33,5 +35,25 @@ class LockerView(ctx:Context):View(ctx) {
         fun handleTap(x:Float,y:Float)  {
 
         }
+    }
+    data class Locker(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = r/30
+            paint.color = Color.WHITE
+            canvas.drawCircle(x,y,r,paint)
+            paint.color = Color.parseColor("#01579B")
+            canvas.drawArc(RectF(-r/2,-r/2,r/2,r/2),0.0f,360.0f*scale,true,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawRect(RectF(-r/6,0.0f,r/6,r/3),paint)
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = r/20
+            canvas.save()
+            canvas.translate(-r/8,0.0f)
+            canvas.rotate(-60*scale)
+            canvas.drawArc(RectF(0.0f,-r/4,r/4,r/4),180.0f,180.0f,false,paint)
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r 
     }
 }
