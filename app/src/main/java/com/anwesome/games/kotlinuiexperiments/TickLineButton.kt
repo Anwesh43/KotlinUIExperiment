4957420bb13968e9d0d3ba422dc13e1e7c625df7
Anwesh43 <anweshthecool0@gmail.com>
2017-08-07 17:07:30 +0530

    adding TickLineButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/TickLineButton.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/TickLineButton.kt
@@ -22,4 +22,15 @@ class TickLineButtonView(ctx:Context,var n:Int=5):View(ctx) {
         }
         return true
     }
+    data class TickLineButton(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            canvas.save()
+            canvas.translate(x,y-canvas.height/2*scale)
+            canvas.rotate(360.0f*scale)
+            canvas.drawCircle(0.0f,0.0f,size/2,paint)
+            canvas.drawLine(0.0f,0.0f,0.0f,-size/3,paint)
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-size/2 && x<=this.x+size/2 && y>=this.y-size/2 && y<=this.y+size/2
+    }
 }
