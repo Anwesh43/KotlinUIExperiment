8c46795550c0e14ca38318995964acc5134699f6
Anwesh43 <anweshthecool0@gmail.com>
2017-08-12 12:06:12 +0530

    adding TickSwitchBoxView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/TickBoxSwitchView.kt
@@ -0,0 +1,20 @@
+package com.anwesome.games.kotlinuiexperiments
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by anweshmishra on 12/08/17.
+ */
+class TickBoxSwitchView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return true
+    }
+}
