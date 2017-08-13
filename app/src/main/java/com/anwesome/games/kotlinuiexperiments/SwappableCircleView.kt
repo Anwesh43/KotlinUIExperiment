8e1190efe210f29c396ee382379fe16e9737dc12
Anwesh43 <anweshthecool0@gmail.com>
2017-08-14 03:37:17 +0530

    adding SwappableViewRenderer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/SwappableCircleView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/SwappableCircleView.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.ConcurrentLinkedQueue
 /**
  * Created by anweshmishra on 14/08/17.
  */
-class SwappableCircleView(ctx:Context):View(ctx) {
+class SwappableCircleView(ctx:Context,var n:Int = 3):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas: Canvas) {
 
@@ -39,11 +39,14 @@ class SwappableCircleView(ctx:Context):View(ctx) {
         fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
     }
     data class TraversePath(var x:Float,var y:Float,var r:Float,var deg:Float) {
+        var updeg = 0.0f
         var traversePoint = PointF()
         fun update() {
-            traversePoint.x = x+r*(Math.cos(deg*Math.PI/180)).toFloat()
-            traversePoint.y = y+r*(Math.sin(deg*Math.PI/180)).toFloat()
+            traversePoint.x = x+r*(Math.cos((deg+updeg)*Math.PI/180)).toFloat()
+            traversePoint.y = y+r*(Math.sin((deg+updeg)*Math.PI/180)).toFloat()
+            updeg += 20.0f
         }
+        fun stopped():Boolean =  updeg > 180.0f
     }
     data class SwapableCircleRenderController(var circles:ConcurrentLinkedQueue<SwappableCircle>,var h:Float,var v:SwappableCircleView) {
         var first:SwappableCircle?=null
@@ -97,6 +100,30 @@ class SwappableCircleView(ctx:Context):View(ctx) {
                     }
                 }
             }
+        }
+    }
+    class SwappableViewRenderer {
+        var time = 0
+        var controller:SwapableCircleRenderController?=null
+        fun render(canvas: Canvas,paint: Paint,v:SwappableCircleView) {
+            if(time == 0) {
+                var w = canvas.width.toFloat()
+                var h = canvas.height.toFloat()
+                var gap = w/(2*v.n+1)
+                var x = 3*gap/2
+                var y = h/2
+                var circles:ConcurrentLinkedQueue<SwappableCircle> = ConcurrentLinkedQueue()
+                for(i in 0..v.n) {
+                    circles.add(SwappableCircle(x,y,2*gap/3))
+                }
+                controller = SwapableCircleRenderController(circles,h,v)
+            }
+            controller?.draw(canvas,paint)
+            controller?.update()
+            time++
+        }
+        fun handleTap(x:Float,y:Float) {
+            controller?.handleTap(x,y)
         }
     }
 }
