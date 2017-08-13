f8f7feffc05dc134c620a9c1260a9523c3a78798
Anwesh43 <anweshthecool0@gmail.com>
2017-08-14 03:11:28 +0530

    adding SwappableCircle

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/SwappableCircleView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/SwappableCircleView.kt
@@ -1,8 +1,7 @@
 package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +20,20 @@ class SwappableCircleView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class SwappableCircle(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            paint.color = Color.parseColor("#00838F")
+            canvas.drawCircle(0.0f,0.0f,r,paint)
+            canvas.restore()
+        }
+        fun update() {
+            var point = PointF()
+            x = point.x
+            y = point.y
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
     }
 }
