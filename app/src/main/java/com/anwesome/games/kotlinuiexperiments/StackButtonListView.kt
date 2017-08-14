2dc274581d7b5ca3e42962ff93b6e2c0d8b77473
Anwesh43 <anweshthecool0@gmail.com>
2017-08-15 00:54:33 +0530

    adding StackButtonShape

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/StackButtonListView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/StackButtonListView.kt
@@ -2,7 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +23,17 @@ class StackButton(ctx:Context,var color:Int,var text:String):View(ctx) {
             }
         }
         return true
+    }
+    data class StackButtonShape(var x:Float,var y:Float,var w:Float,var h:Float,var color:Int,var text:String) {
+        fun draw(canvas: Canvas,paint:Paint) {
+            paint.textSize = h/3
+            paint.color = color
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawRect(RectF(0.0f,0.0f,w,h),paint)
+            paint.color = Color.WHITE
+            canvas.drawText(text,-paint.measureText(text)/2,0.0f,paint)
+            canvas.restore()
+        }
     }
 }
