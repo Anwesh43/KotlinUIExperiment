e917fb47d616fdce625b1cd8ad92266a10bd7684
Anwesh43 <anweshthecool0@gmail.com>
2017-08-16 11:16:04 +0530

    adding SearchButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/SearchButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/SearchButtonView.kt
@@ -2,6 +2,7 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
@@ -12,7 +13,7 @@ import android.view.View
 class SearchButtonView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
     }
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when(event.action) {
@@ -21,5 +22,23 @@ class SearchButtonView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class SearchButton(var x:Float,var y:Float,var w:Float,var h:Float,var deg:Float = 0.0f) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = Color.WHITE
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = w/20
+            paint.strokeCap = Paint.Cap.ROUND
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(deg)
+            canvas.drawLine(0.0f,h/2,0.0f,0.0f,paint)
+            canvas.drawCircle(0.0f,-h/4,h/4,paint)
+            canvas.restore()
+        }
+        fun update(scale:Float) {
+            deg = -45*scale
+            x = w*scale
+        }
     }
 }
