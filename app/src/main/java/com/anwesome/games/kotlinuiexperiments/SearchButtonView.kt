40755b086b967da1ec2613e0841d43dc47455cb8
Anwesh43 <anweshthecool0@gmail.com>
2017-08-16 11:28:42 +0530

    adding DrawingController

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/SearchButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/SearchButtonView.kt
@@ -40,6 +40,7 @@ class SearchButtonView(ctx:Context):View(ctx) {
             deg = -45*scale
             x = w*scale
         }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-h/2 && x<=this.x+h/2 && y>=this.y-h/2 && y<=this.y+h/2
     }
     data class SearchButtonState(var scale:Float = 0.0f,var deg:Float = 0.0f) {
         fun update() {
@@ -50,5 +51,31 @@ class SearchButtonView(ctx:Context):View(ctx) {
             }
         }
         fun stopped():Boolean = deg == 0.0f
+    }
+    class SearchButtonDrawingController(var searchButton:SearchButton,var view:SearchButtonView,var state:SearchButtonState = SearchButtonState(),var animated:Boolean = false) {
+        fun update() {
+            if(animated) {
+                state.update()
+                if(state.stopped()) {
+                    animated = false
+                }
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch (ex:Exception) {
+
+                }
+            }
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            searchButton.draw(canvas,paint)
+        }
+        fun handleTap(x:Float,y:Float) {
+            if(searchButton.handleTap(x,y)) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
     }
 }
