d5e6f4f2ede5ebfaf75f7c5ad43a95dd4834c26c
Anwesh43 <anweshthecool0@gmail.com>
2017-08-17 13:45:06 +0530

    adding BallUp

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/BallUpView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/BallUpView.kt
@@ -2,6 +2,7 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
@@ -21,5 +22,24 @@ class BallUpView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class BallUp(var x:Float,var y:Float,var r:Float,var h:Float) {
+        var origY = 0.0f
+        init {
+            origY = y
+        }
+
+        fun draw(canvas: Canvas, paint: Paint) {
+            canvas.save()
+            canvas.translate(x, y)
+            paint.color = Color.parseColor("#ef5350")
+            canvas.drawCircle(0.0f, 0.0f, r, paint)
+            canvas.restore()
+        }
+
+        fun update(scale: Float) {
+            y = origY - h * scale
+        }
+        fun handleTap(x: Float, y: Float): Boolean = x >= this.x - r && x <= this.x + r && y >= this.y - r && y <= this.y + r
     }
 }
