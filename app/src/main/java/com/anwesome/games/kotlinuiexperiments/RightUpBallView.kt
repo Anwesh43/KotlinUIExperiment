988e6d314bf9f38cdca6ea95553d261b4ac4be93
Anwesh43 <anweshthecool0@gmail.com>
2017-08-19 05:28:07 +0530

    adding RightUpBallView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RightUpBallView.kt
@@ -0,0 +1,25 @@
+package com.anwesome.games.kotlinuiexperiments
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by anweshmishra on 19/08/17.
+ */
+class RightUpBallView(ctx:Context,var n:Int = 6):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean  {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
