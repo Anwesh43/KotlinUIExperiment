0430741a24db33f08420b4402a78f7ec1477778d
Anwesh43 <anweshthecool0@gmail.com>
2017-08-18 13:34:19 +0530

    adding BCLBall

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/BallCircleLayoutView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/BallCircleLayoutView.kt
@@ -2,6 +2,7 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
@@ -21,5 +22,25 @@ class BallCircleLayoutView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class BCLBall(var cx:Float,var cy:Float,var deg:Float,var r:Float,var size:Float,var r1:Float) {
+        var x = 0.0f
+        var y = 0.0f
+        init {
+            x = cx+(r*Math.cos(deg*Math.PI/180).toFloat())
+            y = cy+(r*Math.sin(deg*Math.PI/180).toFloat())
+        }
+        fun draw(canvas:Canvas,paint: Paint) {
+            paint.color = Color.RED
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawCircle(0.0f,0.0f,size/2,paint)
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
+        fun update() {
+
+        }
+        fun stopped():Boolean = true
     }
 }
