03356680f687bba61e94be9f3d78c9a146d45db3
Anwesh43 <anweshthecool0@gmail.com>
2017-08-19 05:33:04 +0530

    adding RUPBall

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RightUpBallView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RightUpBallView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +22,18 @@ class RightUpBallView(ctx:Context,var n:Int = 6):View(ctx) {
             }
         }
         return true
+    }
+    data class RUPBall(var x:Float,var y:Float,var r:Float,var h:Float,var w:Float) {
+        fun draw(canvas: Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawCircle(0.0f,0.0f,r,paint)
+            canvas.restore()
+        }
+        fun update(xscale:Float,yscale:Float) {
+            x = w*xscale
+            y = h*yscale
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
     }
 }
