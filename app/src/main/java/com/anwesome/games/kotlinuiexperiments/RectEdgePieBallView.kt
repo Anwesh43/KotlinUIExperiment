3e50c255ec2ad2513ecf773c9d4f8e37b7d537dc
Anwesh43 <anweshthecool0@gmail.com>
2017-08-21 01:01:08 +0530

    adding RectEdgePieBallView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgePieBallView.kt
@@ -0,0 +1,24 @@
+package com.anwesome.games.kotlinuiexperiments
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by anweshmishra on 21/08/17.
+ */
+class RectEdgePieBallView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas: Canvas) {
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
