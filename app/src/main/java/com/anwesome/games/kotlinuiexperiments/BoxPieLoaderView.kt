cc2d899334ed4f451386dc713c9f716219a1e192
Anwesh43 <anweshthecool0@gmail.com>
2017-08-20 12:38:55 +0530

    adding BoxPieLoaderView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/BoxPieLoaderView.kt
@@ -0,0 +1,23 @@
+package com.anwesome.games.kotlinuiexperiments
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by anweshmishra on 20/08/17.
+ */
+class BoxPieLoaderView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas: Canvas) {
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN ->{
+
+            }
+        }
+    }
+}
