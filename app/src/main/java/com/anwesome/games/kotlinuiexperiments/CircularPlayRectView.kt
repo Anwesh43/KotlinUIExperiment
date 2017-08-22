8a1cbe5928c32ad209e55c4df812471a9a1fe11f
Anwesh43 <anweshthecool0@gmail.com>
2017-08-22 07:33:35 +0530

    adding CirularPlayRect

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircularPlayRectView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircularPlayRectView.kt
@@ -1,8 +1,7 @@
 package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +20,47 @@ class CircularPlayRectView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class CircularPlayRect(var w:Float,var h:Float,var scale:Float = 0.0f) {
+        var circularPlay:CircularPlay?=null
+        init {
+            circularPlay = CircularPlay(w/2,h/2,Math.min(w,h)/5)
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            circularPlay?.draw(canvas,paint,scale)
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            canvas.scale(scale,1.0f)
+            canvas.drawRect(RectF(-w/2,-h/2,w/2,h/2),paint)
+            canvas.restore()
+        }
+        fun update(scale:Float) {
+            this.scale = scale
+        }
+        fun handleTap(x:Float,y:Float):Boolean = circularPlay?.handleTap(x,y)?:false
+    }
+    data class CircularPlay(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            canvas.save()
+            canvas.translate(x,y)
+            var path = Path()
+            path.moveTo(-r/6,-r/6)
+            path.lineTo(-r/6,r/6)
+            path.lineTo(r/6,0.0f)
+            paint.style = Paint.Style.STROKE
+            paint.color = Color.WHITE
+            canvas.drawPath(path,paint)
+            canvas.save()
+            canvas.scale(scale,scale)
+            paint.style = Paint.Style.FILL
+            canvas.drawPath(path, paint)
+            canvas.restore()
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(0.0f,0.0f,r,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-r,-r,r,r),0.0f,360.0f*scale,true,paint)
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y <= this.y+r
     }
 }
