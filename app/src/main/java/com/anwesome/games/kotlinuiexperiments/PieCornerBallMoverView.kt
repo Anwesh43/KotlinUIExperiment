06774f928b30b705f5d7bf57487a93744dd85a09
Anwesh43 <anweshthecool0@gmail.com>
2017-08-23 19:55:56 +0530

    Adding PieCornerBallMover

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieCornerBallMoverView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieCornerBallMoverView.kt
@@ -2,7 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +23,28 @@ class PieCornerBallMoverView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class PieCornerBallMover(var w:Float,var h:Float,var r:Float=Math.min(w,h)/10,var scale:Float = 0.0f) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = Color.parseColor("#26A69A")
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            paint.strokeWidth = r/8
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(0.0f,0.0f,r,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-r,-r,r,r),0.0f,360.0f*scale,true,paint)
+            for(i in 0..3) {
+                canvas.save()
+                canvas.rotate(i*90.0f+45.0f)
+                canvas.drawCircle(0.0f,-h/3*scale,r,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.w/2-r && x<=this.w/2+r && y>=this.h/2 -r && y<=this.h/2+r
+        fun update(scale:Float) {
+            this.scale = scale
+        }
     }
 }
