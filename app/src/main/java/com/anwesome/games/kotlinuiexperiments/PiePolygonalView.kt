20864d3b72e2daba4c31408d882b4502a5a4e250
Anwesh43 <anweshthecool0@gmail.com>
2017-08-24 11:38:38 +0530

    Using PiePolygonal

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/PiePolygonalView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PiePolygonalView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +22,26 @@ class PiePolygonalView(ctx:Context,var n:Int = 3):View(ctx) {
             }
         }
         return true
+    }
+    data class PiePolygonal(var w:Float,var h:Float,var n:Int,var scale:Float = 0.0f,var r:Float = Math.min(w,h)/10) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = Math.min(w,h)/50
+            canvas.drawCircle(0.0f,0.0f,r,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-r,-r,r,r),0.0f,360.0f*scale,true,paint)
+            var deg = 360.0f/n
+            for(i in 1..n) {
+                canvas.save()
+                var x_gap =  (w/3*Math.cos((i*deg/2)*Math.PI/180)).toFloat()
+                var y_gap = (w/3*Math.sin((i*deg/2)*Math.PI/180)).toFloat()
+                canvas.drawLine(-x_gap*scale,y_gap,x_gap*scale,y_gap,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float) = x>=w/2-r && x<=w/2+r && y>=h/2-r && y<=h/2+r
     }
 }
