69f8e4ea995481b9908dd62ea300e0be9cae1789
Anwesh43 <anweshthecool0@gmail.com>
2017-08-26 02:26:39 +0530

    using PieLineDotView in activity

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/MainActivity.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/MainActivity.kt
@@ -85,7 +85,8 @@ class MainActivity : AppCompatActivity() {
 //        RectEdgePieBallView.addSelectionListener(REBSelectionListener(this))
        // CircularPlayRectView.create(this)
         //PieCornerBallMoverView.create(this,PieCornerSelectionListener(this))
-        PiePolygonalView.create(this,6)
+       // PiePolygonalView.create(this,6)
+        PieLineDotView.create(this)
     }
 }
 //data class CompletionListener(var activity: MainActivity):CircleCreatorView.OnCompletionListener {
