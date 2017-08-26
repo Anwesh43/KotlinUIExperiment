5736f699b361970c1be2a8ba874157c78adca139
Anwesh43 <anweshthecool0@gmail.com>
2017-08-27 02:50:10 +0530

    Adding MultiCirucalrButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiCircularButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiCircularButtonView.kt
@@ -6,11 +6,12 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
+import java.util.concurrent.ConcurrentLinkedQueue
 
 /**
  * Created by anweshmishra on 27/08/17.
  */
-class MultiCircularButtonView(ctx:Context):View(ctx) {
+class MultiCircularButtonView(ctx:Context,var n:Int = 6):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
 
@@ -42,5 +43,66 @@ class MultiCircularButtonView(ctx:Context):View(ctx) {
             canvas.restore()
         }
         fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<this.x+r && y>=this.y-r && y<=this.y+r
+    }
+    data class CircularButton(var x:Float,var y:Float,var r:Float,var scale:Float = 0f) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            paint.color = Color.parseColor("#304FFE")
+            canvas.drawCircle(0f,0f,r,paint)
+            canvas.save()
+            canvas.scale(scale,scale)
+            paint.color = Color.argb(130,255,255,255)
+            canvas.drawCircle(0f,0f,r,paint)
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update() {
+            scale+=0.1f
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<this.x+r && y>=this.y-r && y<=this.y+r
+    }
+    data class MultiCircularButton(var cx:Float,var cy:Float,var finalR:Float,var r:Float=0f,var n:Int,var gapDeg:Float = 360f/n) {
+        var circularButtons:ConcurrentLinkedQueue<CircularButton> = ConcurrentLinkedQueue()
+        var tappedButtons:ConcurrentLinkedQueue<CircularButton> = ConcurrentLinkedQueue()
+        init {
+            var gapDeg = 360f/n
+            var r1 = r/5
+            for(i in 0..n-1) {
+                var deg = i*gapDeg
+                var x = cx+(r-r1)*Math.cos(deg*Math.PI/180).toFloat()
+                var y = cy+(r-r1)*Math.sin(deg*Math.PI/180).toFloat()
+                var circularButton = CircularButton(x,y,r1)
+                circularButtons.add(circularButton)
+            }
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            circularButtons.forEach{ circularButton ->
+                circularButton.draw(canvas,paint)
+            }
+        }
+        fun update(scale:Float) {
+            r = finalR*scale
+            var r1 = r/5
+            var deg = 0f
+            circularButtons.forEach { circularButton ->
+                circularButton.x = cx+(r-r1)*Math.cos(deg*Math.PI/180).toFloat()
+                circularButton.y = cy+(r-r1)*Math.sin(deg*Math.PI/180).toFloat()
+                deg+=gapDeg
+            }
+        }
+        fun updateTappedButon() {
+            tappedButtons.forEach {
+
+            }
+        }
+        fun handleTap(x:Float,y:Float):Boolean {
+            circularButtons.forEach { cb ->
+                if(cb.handleTap(x,y)) {
+                    return true
+                }
+            }
+            return false
+        }
     }
 }
