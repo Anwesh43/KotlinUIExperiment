119491a85d928310304d192521a31fb6ee5ed1db
Anwesh43 <anweshthecool0@gmail.com>
2017-08-27 02:21:37 +0530

    Adding ControlButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiCircularButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiCircularButtonView.kt
@@ -2,6 +2,7 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
@@ -21,5 +22,25 @@ class MultiCircularButtonView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class ControlButton(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(45*scale)
+            paint.color = Color.parseColor("#304FFE")
+            canvas.drawCircle(0f,0f,r,paint)
+            paint.color = Color.WHITE
+            paint.strokeWidth = r/30
+            paint.strokeCap = Paint.Cap.ROUND
+            for(i in 0..1) {
+                canvas.save()
+                canvas.rotate(90f*i)
+                canvas.drawLine(0f,-2*r/3,0f,2*r/3,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<this.x+r && y>=this.y-r && y<=this.y+r
     }
 }
