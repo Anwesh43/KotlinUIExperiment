411023b81e4fe50c1011f03bb30f395ef4a70346
Anwesh43 <anweshthecool0@gmail.com>
2017-08-28 03:14:09 +0530

    using BiDirecRenderer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/BiDirecDotView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/BiDirecDotView.kt
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
@@ -15,18 +16,20 @@ import android.view.View
 
 class BiDirecDotView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    var renderer = BiDirecRenderer(this)
     override fun onDraw(canvas: Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
     }
-    class BDDVAnimator():AnimatorListenerAdapter(),ValueAnimator.AnimatorUpdateListener {
+    class BDDVAnimator(var renderer: BiDirecRenderer):AnimatorListenerAdapter(),ValueAnimator.AnimatorUpdateListener {
         var anim = ValueAnimator.ofFloat(0.0f,1.0f)
         var currAnim = anim
         var reverseAnim = ValueAnimator.ofFloat(1.0f,0.0f)
@@ -43,7 +46,8 @@ class BiDirecDotView(ctx:Context):View(ctx) {
         }
         override fun onAnimationUpdate(vf:ValueAnimator) {
             if(animated) {
-
+                var factor = vf.animatedValue as Float
+                renderer.update(factor*(1-mode),factor*mode)
             }
         }
         override fun onAnimationEnd(animation: Animator) {
@@ -76,10 +80,10 @@ class BiDirecDotView(ctx:Context):View(ctx) {
     }
     data class BiDirecDot(var x:Float,var y:Float,var r:Float,var w:Float,var scale1:Float=0.0f,var scale2:Float =0.0f) {
         fun draw(canvas:Canvas,paint:Paint) {
-            for(i in 0..2) {
+            for(i in 0..1) {
                 canvas.save()
                 canvas.translate(x, y)
-                canvas.scale(i-1f,0f)
+                canvas.scale(2*i-1f,0f)
                 canvas.drawCircle(-(w / 2) * scale2, (-w / 2) * scale2, r, paint)
                 canvas.save()
                 canvas.scale(scale1, scale1)
@@ -94,5 +98,28 @@ class BiDirecDotView(ctx:Context):View(ctx) {
             this.scale2 = scale2
         }
         fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x +r  && y>=this.y-r && y<=this.y+r
+    }
+    class BiDirecRenderer(var view:BiDirecDotView) {
+        var time = 0
+        var bidirecdot:BiDirecDot?=null
+        var animator = BDDVAnimator(this)
+        fun render(canvas:Canvas,paint:Paint) {
+            if(time == 0) {
+                var w = canvas.width.toFloat()
+                var h = canvas.height.toFloat()
+                bidirecdot = BiDirecDot(w/2,h/2,Math.min(w,h)/20,w-Math.min(w,h)/20)
+            }
+            bidirecdot?.draw(canvas,paint)
+            time++
+        }
+        fun update(scale1: Float,scale2: Float) {
+            bidirecdot?.update(scale1,scale2)
+            view.postInvalidate()
+        }
+        fun handleTap(x:Float,y:Float) {
+            if(bidirecdot?.handleTap(x,y)?:false) {
+                animator.start()
+            }
+        }
     }
 }
