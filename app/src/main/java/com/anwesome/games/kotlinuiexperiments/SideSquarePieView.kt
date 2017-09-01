55174dbbe13ed5aebeb1ba61f4b52bb32da8c432
Anwesh43 <anweshthecool0@gmail.com>
2017-09-02 02:40:43 +0530

    adding SideSquarePie

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/SideSquarePieView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/SideSquarePieView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -22,5 +23,39 @@ class SideSquarePieView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class SideSquarePie(var w:Float,var h:Float,var i:Int,var scale:Float = 0f) {
+        var r = Math.min(w,h)/15
+        var x = (w/2-2*r)+(i%2)*4*r
+        var y = (h/2 -2*r)+(i/2)*4*r
+        fun draw(canvas:Canvas,paint:Paint) {
+            var rx = Math.min(w,h)/12
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            canvas.save()
+            canvas.rotate(90f*i)
+            paint.style = Paint.Style.STROKE
+            canvas.drawRoundRect(RectF(-2*w/5,-2*h/5,2*w/5,-2*h/5+h/10),rx,rx,paint)
+            paint.style = Paint.Style.FILL
+            canvas.save()
+            canvas.translate(-2*w/5,-2*h/5+h/20)
+            canvas.scale(scale,1f)
+            canvas.drawRoundRect(RectF(0f,-h/20,4*w/5,h/20),rx,rx,paint)
+            canvas.restore()
+            canvas.restore()
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(x-w/2,y-h/2,r,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-r,-r,r,r),0f,360f*scale,true,paint)
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x - r && x<=this.x+r && y>=this.y-r && y<=this.y+r
+        fun update() {
+
+        }
+        fun startUpdate() {
+
+        }
+        fun stopped():Boolean = false
     }
 }
