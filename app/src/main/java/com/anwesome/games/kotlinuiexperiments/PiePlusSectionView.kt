29c5f47291cfaa1599a20a5823acd0d0ccd8eb49
Anwesh43 <anweshthecool0@gmail.com>
2017-09-03 01:56:04 +0530

    adding PiePlusSection

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/PiePlusSectionView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PiePlusSectionView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +22,35 @@ class PiePlusSectionView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class PiePlusSection(var i:Int,var x:Float,var y:Float,var r:Float) {
+        var lx = x - r + r*(i%2)
+        var ly = y - r + r*(i/2)
+        var ux = x + r*(i%2)
+        var uy = y + r*(i/2)
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawArc(RectF(-r,-r,r,r),(180+i*90f),90f,true,paint)
+            canvas.save()
+            canvas.translate(-r/2+r*(i%2),-r/2+r*(i/2))
+            canvas.rotate(45f)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.rotate(90f*i)
+                canvas.drawLine(-r/10,0f,r/10,0f,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=lx && y>=ly && x<=ux && y<=uy
+        fun update() {
+
+        }
+        fun stopped():Boolean = false
+        fun startUpdating() {
+
+        }
     }
 }
