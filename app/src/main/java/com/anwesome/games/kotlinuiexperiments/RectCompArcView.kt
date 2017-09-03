39e916feb1fc5f486c4db3bd7db8a6a52046679f
Anwesh43 <anweshthecool0@gmail.com>
2017-09-04 00:38:50 +0530

    adding state class

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectCompArcView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectCompArcView.kt
@@ -2,6 +2,7 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
 import android.view.MotionEvent
@@ -23,14 +24,15 @@ class RectCompArcView(ctx:Context):View(ctx) {
         }
         return true
     }
-    data class RectCompArc(var w:Float,var h:Float) {
+    data class RectCompArc(var w:Float,var h:Float,var state:RCAState = RCAState()) {
         fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = Color.parseColor("#3F51B5")
             for(i in 0..1) {
                 canvas.save()
                 canvas.translate(w / 2, h / 2)
                 canvas.scale(1f-2*i,1f)
-                canvas.drawArc(RectF(-0.45f*w,-0.05f*w,-0.35f*w,0.05f*w),0f,360f,true,paint)
-                canvas.drawRect(RectF(-0.35f*w,-0.05f*w,-0.35f*w+0.35f*w,0.05f*w),paint)
+                canvas.drawArc(RectF(-0.45f*w,-0.05f*w,-0.35f*w,0.05f*w),0f,360f*state.scale,true,paint)
+                canvas.drawRect(RectF(-0.35f*w,-0.05f*w,-0.35f*w+(0.35f*w)*(1-this.state.scale),0.05f*w),paint)
                 canvas.restore()
             }
         }
@@ -41,5 +43,24 @@ class RectCompArcView(ctx:Context):View(ctx) {
 
         }
         fun stopped():Boolean = false
+    }
+    data class RCAState(var scale:Float = 0f,var dir:Int = 0) {
+        fun update() {
+            scale += dir*0.1f
+            if(scale > 1) {
+                scale = 1f
+                dir = 0
+            }
+            if(scale < 0) {
+                dir = 0
+                scale = 0f
+            }
+        }
+        fun stopped():Boolean {
+            return dir == 0
+        }
+        fun startUpdating() {
+            dir = (1-2*scale).toInt()
+        }
     }
 }
