c92d60527610c219d1cba4c0edfac1d7413b7944
Anwesh43 <anweshthecool0@gmail.com>
2017-09-04 00:33:42 +0530

    adding RectCompArc

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectCompArcView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectCompArcView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +22,24 @@ class RectCompArcView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class RectCompArc(var w:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            for(i in 0..1) {
+                canvas.save()
+                canvas.translate(w / 2, h / 2)
+                canvas.scale(1f-2*i,1f)
+                canvas.drawArc(RectF(-0.45f*w,-0.05f*w,-0.35f*w,0.05f*w),0f,360f,true,paint)
+                canvas.drawRect(RectF(-0.35f*w,-0.05f*w,-0.35f*w+0.35f*w,0.05f*w),paint)
+                canvas.restore()
+            }
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = false
     }
 }
