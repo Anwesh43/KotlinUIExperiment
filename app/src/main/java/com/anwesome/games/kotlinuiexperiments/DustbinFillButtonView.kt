aeefe663c50c8667d474e1a8f158cce8895557dc
Anwesh43 <anweshthecool0@gmail.com>
2017-09-06 02:51:55 +0530

    adding create method

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DustbinFillButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DustbinFillButtonView.kt
@@ -1,9 +1,11 @@
 package com.anwesome.games.kotlinuiexperiments
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by anweshmishra on 06/09/17.
@@ -118,6 +120,13 @@ class DustbinFillButtonView(ctx:Context):View(ctx) {
         }
         fun handleTap() {
             animator?.handleTap()
+        }
+    }
+    companion object {
+        fun create(activity:Activity) {
+            var view = DustbinFillButtonView(activity)
+            var size = DimensionsUtil.getDimension(activity)
+            activity.addContentView(view, ViewGroup.LayoutParams(size.x/2,size.x/2))
         }
     }
 }
