b76635a929e8d76112dfaa0fc4961b57c74c4a60
Anwesh43 <anweshthecool0@gmail.com>
2017-09-06 02:45:31 +0530

    adding Dustbin state and using it

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DustbinFillButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DustbinFillButtonView.kt
@@ -21,7 +21,7 @@ class DustbinFillButtonView(ctx:Context):View(ctx) {
         }
         return true
     }
-    data class DustbinFill(var w:Float,var h:Float) {
+    data class DustbinFill(var w:Float,var h:Float,var state:DustbinState= DustbinState()) {
         fun draw(canvas:Canvas,paint:Paint) {
             paint.color = Color.parseColor("#9E9E9E")
             drawBin(canvas, paint,Color.parseColor("#757575"))
@@ -49,11 +49,28 @@ class DustbinFillButtonView(ctx:Context):View(ctx) {
             canvas.drawArc(RectF(x1-r1,y1-r2,x1+r1,y1+r1),0f,360f,true,paint)
         }
         fun update() {
-
+            state.update()
         }
         fun startUpdating() {
-
+            state.startUpdate()
         }
-        fun stopped():Boolean = false
+        fun stopped():Boolean = state.stopped()
+    }
+    data class DustbinState(var scale:Float = 0f,var dir:Float = 0f) {
+        fun update() {
+            scale += dir * 0.1f
+            if(scale > 1) {
+                scale = 1f
+                dir = 0f
+            }
+            if(scale < 0) {
+                scale = 0f
+                dir = 0f
+            }
+        }
+        fun stopped():Boolean = dir == 0f
+        fun startUpdate() {
+            dir = 1f-2*scale
+        }
     }
 }
