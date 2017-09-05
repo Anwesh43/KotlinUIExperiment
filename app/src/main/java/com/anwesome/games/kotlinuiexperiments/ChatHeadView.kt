2be9175efc1bd85f31b71f4a44e65583c04e0d6a
Anwesh43 <anweshthecool0@gmail.com>
2017-09-05 09:10:13 +0530

    creating and using state

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ChatHeadView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ChatHeadView.kt
@@ -21,7 +21,7 @@ class ChatHeadView(ctx:Context):View(ctx) {
         }
         return true
     }
-    data class ChatHead(var size:Float) {
+    data class ChatHead(var size:Float,var state:ChatHeadState = ChatHeadState()) {
         fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(4*size/5,4*size/5)
@@ -29,7 +29,7 @@ class ChatHeadView(ctx:Context):View(ctx) {
             canvas.drawCircle(0f,0f,size/10,paint)
             for(i in 0..1) {
                 canvas.save()
-                canvas.rotate(90f*i+45f)
+                canvas.rotate(90f*i+45f*this.state.scale)
                 paint.strokeWidth = size/40
                 canvas.drawLine(0f,-size/30,0f,size/30,paint)
                 canvas.restore()
@@ -38,7 +38,7 @@ class ChatHeadView(ctx:Context):View(ctx) {
             canvas.save()
             canvas.translate(size/10,size/10)
             var clipPath = Path()
-            clipPath.addRect(RectF(0f,0.6f*size*(1),0.6f*size,0.6f*size),Path.Direction.CCW)
+            clipPath.addRect(RectF(0f,0.6f*size*(1-this.state.scale),0.6f*size,0.6f*size),Path.Direction.CCW)
             canvas.clipPath(clipPath)
             canvas.drawRoundRect(RectF(0f,0f,size*0.6f,size*0.5f),size/10,size/10,paint)
             var path = Path()
@@ -52,11 +52,28 @@ class ChatHeadView(ctx:Context):View(ctx) {
             return x>=0.7f*size && x<=0.9f*size && y>=0.7f*size && y<=0.9f*size
         }
         fun update() {
-
+            this.state.update()
         }
         fun startUpdating() {
-
+            this.state.startUpdating()
         }
-        fun stopped():Boolean = false
+        fun stopped():Boolean = this.state.stopped()
+    }
+    data class ChatHeadState(var scale:Float = 0f,var dir:Float=0f) {
+        fun update() {
+            scale += dir*0.1f
+            if(scale > 1) {
+                dir = 0f
+                scale = 1f
+            }
+            if(scale < 0) {
+                scale = 0f
+                dir = 0f
+            }
+        }
+        fun stopped():Boolean = dir == 0f
+        fun startUpdating() {
+            dir = 1-2*scale
+        }
     }
 }
