aa2ef3220ecd132fb28f9fba1b4218c770eb31ea
Anwesh43 <anweshthecool0@gmail.com>
2017-09-07 02:31:48 +0530

    adding AtomButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/AtomButtonVuew.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/AtomButtonVuew.kt
@@ -2,7 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -20,5 +22,29 @@ class AtomButtonView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class AtomButton(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            paint.color = Color.parseColor("#009688")
+            canvas.drawCircle(0f,0f,r,paint)
+            paint.color = Color.WHITE
+            paint.strokeWidth = r/30
+            for(i in 0..2) {
+                canvas.save()
+                canvas.rotate(i*30f)
+                canvas.drawArc(RectF(-r/2,-r/10,r/2,r/10),0f,360f,true,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
     }
 }
