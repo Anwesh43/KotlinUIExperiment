bbd9c7dbd3a0df46af791edf00cdbfcb5cf8962d
Anwesh43 <anweshthecool0@gmail.com>
2017-09-08 00:40:34 +0530

    adding create method in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ChromeButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ChromeButtonView.kt
@@ -1,5 +1,6 @@
 package com.anwesome.games.kotlinuiexperiments
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -7,19 +8,22 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by anweshmishra on 08/09/17.
  */
 class ChromeButtonView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = ChromeButtonRenderer()
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint,this)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
@@ -110,6 +114,13 @@ class ChromeButtonView(ctx:Context):View(ctx) {
         }
         fun handleTap(x:Float,y:Float) {
             chromeButtonAnimator?.handleTap(x,y)
+        }
+    }
+    companion object {
+        fun create(activity:Activity) {
+            val view = ChromeButtonView(activity)
+            val size = DimensionsUtil.getDimension(activity)
+            activity.addContentView(view, ViewGroup.LayoutParams(size.x/2,size.x/2))
         }
     }
 }
