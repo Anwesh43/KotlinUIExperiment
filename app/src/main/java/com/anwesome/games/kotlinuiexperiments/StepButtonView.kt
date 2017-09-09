8817044c553570d79b34558ed0f1dfafb2e0cb78
Anwesh43 <anweshthecool0@gmail.com>
2017-09-09 22:32:46 +0530

    adding StepButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/StepButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/StepButtonView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -19,5 +20,26 @@ class StepButtonView(ctx:Context):View(ctx) {
 
         }
         return true
+    }
+    data class StepButton(var w:Float,var h:Float,var x:Float = w/2,var y:Float = 0.9f*h,var r:Float = Math.min(w,h)/25) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawArc(RectF(-r,-r,r,r),0f,360f,true,paint)
+            canvas.restore()
+            val size = Math.min(w,h)/10
+            for(i in 1..4) {
+                val x = i*(w/5)
+                canvas.drawRect(RectF(x-size/2,0.8f*h-(size)*i,x+size/2,0.8f*size),paint)
+            }
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x - this.r && x<=this.x+this.r && y>=this.y - this.r && y<=this.y+r
     }
 }
