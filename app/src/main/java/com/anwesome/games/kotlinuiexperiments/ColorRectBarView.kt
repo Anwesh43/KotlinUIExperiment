137caaf10dca35f89dff98b1381310401f7856a4
Anwesh43 <anweshthecool0@gmail.com>
2017-09-10 11:20:47 +0530

    adding ColorBarState

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorRectBarView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorRectBarView.kt
@@ -59,4 +59,21 @@ class ColorRectBarView(ctx:Context):View(ctx) {
         }
         fun stopped():Boolean = true
     }
+    data class ColorRectBarState(var scale:Float = 0f,var dir:Float = 0f) {
+        fun update() {
+            scale += dir * 0.1f
+            if(scale > 1) {
+                scale = 1f
+                dir = 0f
+            }
+            if(scale < 0) {
+                scale = 0f
+                dir = 0f
+            }
+        }
+        fun startUpdating() {
+            dir = 1f - 2*scale
+        }
+        fun stopped():Boolean = dir == 0f
+    }
 }
