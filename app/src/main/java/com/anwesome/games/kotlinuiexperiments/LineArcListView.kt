a97a848172b56df009839b6426fd72d43446795d
Anwesh43 <anweshthecool0@gmail.com>
2017-09-11 09:02:07 +0530

    adding state and using it in LineArc

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/LineArcListView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/LineArcListView.kt
@@ -24,24 +24,31 @@ class LineArcListView(ctx:Context):View(ctx) {
         }
         return true
     }
-    data class LineArc(var x:Float,var y:Float,var r:Float,var h:Float) {
+    data class LineArc(var x:Float,var y:Float,var r:Float,var h:Float,var state:LineArcState = LineArcState()) {
         fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(x,y)
             paint.style = Paint.Style.STROKE
             canvas.drawCircle(0f,0f,r,paint)
             paint.style = Paint.Style.FILL
-            canvas.drawArc(RectF(-r,-r,r,r),0f,360f,true,paint)
-            canvas.drawLine(0f,-2*r,0f,-2*r-h,paint)
+            canvas.drawArc(RectF(-r,-r,r,r),0f,360f*state.scale,true,paint)
+            canvas.drawLine(0f,-2*r,0f,-2*r-h*state.scale,paint)
             canvas.restore()
         }
         fun handleTap(x:Float,y:Float) = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
         fun update() {
-
+            state.update()
         }
-        fun startUpdating() {
-
+        fun stopped():Boolean = state.stopped()
+    }
+    data class LineArcState(var scale:Float = 0f,var deg:Float = 0f) {
+        fun update() {
+            scale  = Math.abs(Math.cos(deg*Math.PI/180)).toFloat()
+            deg += 4.5f
+            if(deg > 180) {
+                deg = 0f
+            }
         }
-        fun stopped():Boolean = false
+        fun stopped():Boolean = deg == 0f
     }
 }
