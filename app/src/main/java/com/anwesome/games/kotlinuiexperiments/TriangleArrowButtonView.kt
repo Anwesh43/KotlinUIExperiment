da8e9f3796d66eb1df353c13496b879e445981d6
Anwesh43 <anweshthecool0@gmail.com>
2017-09-16 04:12:22 +0530

    adding TriangleArrowButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/TriangleArrowButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/TriangleArrowButtonView.kt
@@ -2,7 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +23,31 @@ class  TriangleArrowButtonView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class TriangleArrowButton(var x:Float,var y:Float,var l:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = Color.WHITE
+            canvas.save()
+            canvas.translate(x,y)
+            for(i in 1..4) {
+                canvas.save()
+                canvas.rotate(90f*i)
+                canvas.translate(0f,l)
+                var path = Path()
+                path.moveTo(-r/2,r/2)
+                path.lineTo(r/2,r/2)
+                path.lineTo(0f,-r/2)
+                canvas.drawPath(path,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
     }
 }
