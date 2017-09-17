15c7b66b63e37256b1aaa26a460d0d58e9ab53a9
Anwesh43 <anweshthecool0@gmail.com>
2017-09-17 10:10:35 +0530

    adding PlayBarView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PlayBarView.kt
@@ -0,0 +1,26 @@
+package com.anwesome.games.kotlinuiexperiments
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by anweshmishra on 17/09/17.
+ */
+
+class PlayBarView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
