cd836605ff7183c3a33a20ec5df0b261c4d07993
Anwesh43 <anweshthecool0@gmail.com>
2017-09-18 09:52:35 +0530

    adding static create method

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieLoaderDirectionView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieLoaderDirectionView.kt
@@ -1,5 +1,6 @@
 package com.anwesome.games.kotlinuiexperiments
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -7,6 +8,7 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by anweshmishra on 18/09/17.
@@ -107,6 +109,13 @@ class PieLoaderDirectionView(ctx:Context):View(ctx) {
         }
         fun handleTap(x:Float,y:Float) {
             animator?.handleTap(x,y)
+        }
+    }
+    companion object {
+        fun create(activity:Activity) {
+            var view = PieLoaderDirectionView(activity)
+            var size = DimensionsUtil.getDimension(activity)
+            activity.addContentView(view, ViewGroup.LayoutParams(size.x,size.x))
         }
     }
 }
