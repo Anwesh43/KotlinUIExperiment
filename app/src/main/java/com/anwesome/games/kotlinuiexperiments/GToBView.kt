8b3ca3e358fe440097b2dfd5e6f73c25468e863e
Anwesh43 <anweshthecool0@gmail.com>
2017-09-19 01:37:49 +0530

    adding GToBvIEW

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/GToBView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/GToBView.kt
@@ -1,8 +1,7 @@
 package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
+import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
 
@@ -22,5 +21,35 @@ class GtoBView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class GtoBCircle(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.save()
+            clipRectPath(-r,-r+2*r,canvas)
+            paint.color = Color.GREEN
+            canvas.drawCircle(0f,0f,r,paint)
+            canvas.restore()
+            canvas.save()
+            clipRectPath(r-2*r,r,canvas)
+            paint.color = Color.BLUE
+            canvas.drawCircle(0f,0f,r,paint)
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = false
+        private fun clipRectPath(yStart:Float,yEnd:Float,canvas: Canvas) {
+            val path = Path()
+            path.addRect(RectF(-r,yStart,r,yEnd),Path.Direction.CW)
+            canvas.clipPath(path)
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
     }
 }
