72612780787053ebea920b995abe3c7ff57527c6
Anwesh43 <anweshthecool0@gmail.com>
2017-09-18 09:39:06 +0530

    adding PieDirectionLoader

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieLoaderDirectionView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieLoaderDirectionView.kt
@@ -2,7 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -17,9 +19,34 @@ class PieLoaderDirectionView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    data class PieDirectionLoader(var w:Float,var h:Float) {
+        fun draw(canvas: Canvas,paint:Paint) {
+            val color = Color.parseColor("00ACC1")
+            paint.color = color
+            canvas.save()
+            canvas.translate(w / 2, h / 2)
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(0f, 0f, w / 5, paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-w / 5, -w / 5, w / 5, w / 5), 0f, 360f, true, paint)
+            canvas.restore()
+            canvas.drawLine(-w / 20, 4 * h / 5, -w / 20 + (w), 4 * h / 5, paint)
+            canvas.drawCircle(-w / 20 + (w), 4 * h / 5, w / 40, paint)
+            paint.color = Color.argb(150, Color.red(color), Color.green(color), Color.blue(color))
+            canvas.drawCircle(-w/20+(w),4*h/5,w/30,paint)
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
+        fun handleTap(x:Float,y:Float):Boolean = x>=w/2-w/10 && x<=w/2+w/10 && y>=h/2-w/10 && y<=h/2+w/10
     }
 }
