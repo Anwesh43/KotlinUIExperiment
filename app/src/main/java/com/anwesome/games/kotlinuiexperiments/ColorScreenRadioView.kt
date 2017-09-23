7f8ac1afac7a9a42b963e701684e2e87dd66cbfc
Anwesh43 <anweshthecool0@gmail.com>
2017-09-23 10:11:31 +0530

    adding ColorScreenRadio

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorScreenRadioView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorScreenRadioView.kt
@@ -2,7 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +23,21 @@ class ColorScreenRadioView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class ColorScreenRadio(var w:Float,var h:Float,var rx:Float = w/20,var ry:Float = 19*h/20-w/20,var r:Float = w/20,var orx:Float = rx) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.drawRect(RectF(0f,0f,w,9*h/10),paint)
+            canvas.drawLine(0f,ry,0.9f*w,ry,paint)
+            canvas.drawCircle(rx,ry,r,paint)
+            rx = orx+(9*w/10)
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
+        fun handleTap(x:Float,y:Float):Boolean = x>=rx-r && x<=rx+r && y>=ry-r && y<=ry+r
     }
 }
