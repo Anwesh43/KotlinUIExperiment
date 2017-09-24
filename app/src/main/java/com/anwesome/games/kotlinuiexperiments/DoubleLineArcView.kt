0cb3b09ff1118a610cc92834f8f3f4f165328522
Anwesh43 <anweshthecool0@gmail.com>
2017-09-24 11:27:08 +0530

    adding CircleAlongLine

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DoubleLineArcView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DoubleLineArcView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +22,25 @@ class DoubleLineArcView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class CircleAlongLine(var i:Int,var w:Float,var h:Float,var oy:Float = 0.95f*h/20,var y:Float = oy,var r:Float = h/20,var cr:Float = h/5,var x:Float = w/20+0.9f*w*i) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            canvas.drawArc(RectF(-cr,-cr,cr,cr),i*180f,180f,true,paint)
+            canvas.restore()
+            canvas.save()
+            canvas.drawLine(x,y,x,oy,paint)
+            canvas.drawCircle(x,y,r,paint)
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = false
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
     }
 }
