9a322437260177caa54c552fa08830b5c845da99
Anwesh43 <anweshthecool0@gmail.com>
2017-09-26 11:11:47 +0530

    adding TouchUpFillScreen

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/TouchDownFillScreenView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/TouchDownFillScreenView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +22,26 @@ class TouchDownFillScreenView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class TouchDownFillScreen(var w:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(w/2,0.9f*h)
+            paint.strokeWidth = w/60
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(0f,0f,w/20,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-w/20,-w/20,w/20,w/20),0f,360f,true,paint)
+            canvas.restore()
+            canvas.drawRect(RectF(0f,0f,w,0.8f*h),paint)
+        }
+        fun update() {
+
+        }
+        fun startUpdating(dir:Float) {
+
+        }
+        fun stopped():Boolean = false
+        fun handleTap(x:Float,y:Float):Boolean = x>=w/2 - w/20 && x<=w/2+w/20 && y>=0.9f*h-w/20 && y<=0.9f*h+w/20
     }
 }
