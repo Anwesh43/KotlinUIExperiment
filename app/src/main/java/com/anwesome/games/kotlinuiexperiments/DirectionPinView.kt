d32e75e282397d5a198f8d9021bf34c8ea1aa01a
Anwesh43 <anweshthecool0@gmail.com>
2017-09-28 01:50:21 +0530

    adding DirectionPin

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirectionPinView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirectionPinView.kt
@@ -17,4 +17,24 @@ class DirectionPinView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         return true
     }
+    data class DirectionPin(var w:Float,var h:Float,var dir:Float,var x:Float = w/2,var y:Float = h/2,var size:Float = h/6,var r:Float = h/15) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(90*dir)
+            canvas.save()
+            canvas.translate(0f,w/2)
+            canvas.drawLine(0f,0f,0f,-size,paint)
+            canvas.drawCircle(0f,-size,r,paint)
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating(){
+            
+        }
+        fun stopped():Boolean = false
+    }
 }
