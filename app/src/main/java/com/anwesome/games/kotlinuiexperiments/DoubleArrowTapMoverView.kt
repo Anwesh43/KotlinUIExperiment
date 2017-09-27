4e10df41549126b8ee1a8dd3ec78475a9796d310
Anwesh43 <anweshthecool0@gmail.com>
2017-09-27 09:43:02 +0530

    adding DoubleArrowTapMover

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DoubleArrowTapMoverView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DoubleArrowTapMoverView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +22,44 @@ class DoubleArrowTapMoverView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class DoubleArrowTapMover(var x:Float,var y:Float,var size:Float,var maxGap:Float = 0f,var gap:Float = 0f) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(90f)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.scale(1f,2*i-1f)
+                canvas.save()
+                canvas.translate(0f,gap)
+                canvas.drawPath(arrowPath(),paint)
+                canvas.restore()
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun arrowPath():Path {
+            val path = Path()
+            path.moveTo(-size/2,0f)
+            path.lineTo(0f,-size/2)
+            path.lineTo(size/2,-size/2)
+            return path
+        }
+        fun stopped():Boolean = gap >= maxGap
+        fun update() {
+
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x >= this.x - size/2 && x <= this.x+size/2 && y >= this.y -size/2 && y <= this.y+size/2
+    }
+    data class DoubleArrowTapMoverState(var mode:Int = 0,var scale:Float = 0f) {
+        fun update() {
+            scale += 0.1f
+            if(scale > 1) {
+                mode ++
+                scale = 0f
+            }
+        }
+        fun stopped():Boolean = mode == 2
     }
 }
