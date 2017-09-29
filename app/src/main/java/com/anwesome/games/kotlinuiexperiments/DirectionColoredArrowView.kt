d30ef1c407204bbc6f5fff42e58889ad4ca3e5d4
Anwesh43 <anweshthecool0@gmail.com>
2017-09-30 01:58:47 +0530

    adding DirectionColoredArrow

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirectionColoredArrowView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirectionColoredArrowView.kt
@@ -3,6 +3,8 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +23,28 @@ class DirectionColoredArrowView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class DirectionColoredArrow(var w:Float,var h:Float,var color:Int) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            val path = Path()
+            path.addRect(RectF(0f,0f,w,h),Path.Direction.CW)
+            canvas.drawLine(w/2,h-h/20,w/2,h/20,paint)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.translate(w/2,h/20)
+                canvas.scale(1f-2*i,1f)
+                canvas.drawLine(0f,0f,-w/3,h/5,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = false
     }
 }
