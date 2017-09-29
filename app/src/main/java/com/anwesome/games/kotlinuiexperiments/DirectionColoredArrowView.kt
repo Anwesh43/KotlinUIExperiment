cc3db874ca5172c0d93a64a8e515c2d945865300
Anwesh43 <anweshthecool0@gmail.com>
2017-09-30 02:39:00 +0530

    adding static create method

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirectionColoredArrowView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirectionColoredArrowView.kt
@@ -1,9 +1,11 @@
 package com.anwesome.games.kotlinuiexperiments
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by anweshmishra on 30/09/17.
@@ -126,6 +128,13 @@ class DirectionColoredArrowView(ctx:Context):View(ctx) {
 
         fun handleTap() {
             animator?.handleTap()
+        }
+    }
+    companion object {
+        fun create(activity:Activity) {
+            var view = DirectionColoredArrowView(activity)
+            var size = DimensionsUtil.getDimension(activity)
+            activity.addContentView(view, ViewGroup.LayoutParams(size.x/2,size.x/2))
         }
     }
 }
