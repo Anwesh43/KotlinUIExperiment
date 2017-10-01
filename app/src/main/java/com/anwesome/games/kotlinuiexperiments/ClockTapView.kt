eb9accda180c6f578efdc2c196d17b2afcf09557
Anwesh43 <anweshthecool0@gmail.com>
2017-10-01 12:59:20 +0530

    using renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ClockTapView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ClockTapView.kt
@@ -11,13 +11,14 @@ import android.view.View
  */
 class ClockTapView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = ClockTapRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
