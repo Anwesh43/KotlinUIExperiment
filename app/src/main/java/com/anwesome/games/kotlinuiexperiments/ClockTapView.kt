ffa1afd457ee79d9dc378f3a8dd085dddabc8b32
Anwesh43 <anweshthecool0@gmail.com>
2017-10-01 13:10:22 +0530

    adding static create function to create the view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ClockTapView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ClockTapView.kt
@@ -1,10 +1,12 @@
 package com.anwesome.games.kotlinuiexperiments
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by anweshmishra on 01/10/17.
@@ -112,6 +114,13 @@ class ClockTapView(ctx:Context):View(ctx) {
         }
         fun handleTap(x:Float,y:Float) {
             animator?.handleTap(x,y)
+        }
+    }
+    companion object {
+        fun create(activity:Activity) {
+            val view = ClockTapView(activity)
+            val size = DimensionsUtil.getDimension(activity)
+            activity.addContentView(view,ViewGroup.LayoutParams(size.x/2,size.x/2))
         }
     }
 }
