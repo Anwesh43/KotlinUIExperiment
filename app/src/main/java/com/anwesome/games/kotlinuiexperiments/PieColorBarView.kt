192279905d61244a575bea92f85caab5c1587780
Anwesh43 <anweshthecool0@gmail.com>
2017-10-02 09:19:59 +0530

    using renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieColorBarView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieColorBarView.kt
@@ -11,13 +11,15 @@ import java.util.*
  */
 class PieColorBarView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = PieColorBarRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(x,y)
             }
         }
         return true
