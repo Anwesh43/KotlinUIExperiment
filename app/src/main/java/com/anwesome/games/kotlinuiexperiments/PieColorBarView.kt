376772dd6825907695f2bed67bbd651d63058d15
Anwesh43 <anweshthecool0@gmail.com>
2017-10-02 09:01:20 +0530

    adding PieColorBar

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieColorBarView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PieColorBarView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import android.graphics.*
 import android.view.MotionEvent
+import java.util.*
 
 /**
  * Created by anweshmishra on 02/10/17.
@@ -20,5 +21,44 @@ class PieColorBarView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class ColorBar(var y:Float,var w:Float,var h:Float,var color:Int) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            paint.color = color
+            canvas.drawRect(RectF(0f,0f,w*scale,h),paint)
+        }
+    }
+    data class PieColorBar(var w:Float,var h:Float) {
+        var colors:Array<String> = arrayOf("#f44336","#2196F3","#E64A19","#0097A7","#AD1457")
+        var colorBars:LinkedList<ColorBar> = LinkedList<ColorBar>()
+        init {
+            var y = h/5
+            var yGap = 0.8f*h/colors.size
+            colors.forEach { color ->
+                colorBars.push(ColorBar(y,w,h,Color.parseColor(color)))
+                y += yGap
+            }
+        }
+        fun draw(canvas: Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(w/2,h/10)
+            paint.color = Color.parseColor("#FDD835")
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = Math.min(w,h)/40
+            canvas.drawCircle(0f,0f,h/12,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-h/12,-h/12,h/12,h/12),0f,360f,true,paint)
+            canvas.restore()
+            colorBars.forEach { colorBar ->
+                colorBar.draw(canvas,paint,0f)
+            }
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = false
     }
 }
