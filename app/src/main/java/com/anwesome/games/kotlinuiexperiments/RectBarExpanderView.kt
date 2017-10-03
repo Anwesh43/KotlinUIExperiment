02a361de07f5453124c8aef14ceea60c1600d46d
Anwesh43 <anweshthecool0@gmail.com>
2017-10-03 10:04:58 +0530

    adding RectBarExpander

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectBarExpanderView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectBarExpanderView.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
+
 /**
  * Created by anweshmishra on 03/10/17.
  */
@@ -21,4 +24,30 @@ class RectBarExpanderView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class RectBarExpander(var x:Float,var h:Float,var size:Float,var y:Float = h-size){
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = Color.parseColor("#F57F17")
+            canvas.save()
+            canvas.translate(x,y)
+            paint.strokeWidth = h/50
+            paint.style = Paint.Style.FILL
+            canvas.drawCircle(0f,0f,size,paint)
+            canvas.save()
+            canvas.rotate(90f)
+            canvas.drawLine(-size/2,0f,size/2,0f,paint)
+            canvas.restore()
+            canvas.restore()
+            canvas.drawRect(RectF(x-size,y,x+size,h),paint)
+            y = size+(h-2*size)*(1)
+        }
+        fun update() {
+
+        }
+        fun stopped():Boolean = false
+        fun startUpdating() {
+
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-size && x<=this.x+size && y>=this.y-size && y<=this.y+size
+    }
+
 }
