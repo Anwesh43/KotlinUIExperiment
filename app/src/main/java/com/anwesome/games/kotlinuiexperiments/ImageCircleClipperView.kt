b06b6f668e75c9c05f5948ecd85beb8cac6660f7
Anwesh43 <anweshthecool0@gmail.com>
2017-10-04 00:58:08 +0530

    adding Pixel and drawing it

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ImageCircleClipperView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ImageCircleClipperView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 import java.util.*
@@ -24,22 +25,32 @@ class ImageCircleClipperView(ctx:Context,var bitmap:Bitmap):View(ctx) {
         }
         return true
     }
-    data class ImageClipperView(var bitmap:Bitmap,var size:Float) {
-        var rectPixels:LinkedList<Int> = LinkedList()
-        var circlePixels:LinkedList<Int> = LinkedList()
+    data class ImageClipperView(var bitmap:Bitmap,var size:Float,var cx:Float = bitmap.width.toFloat()/2,var cy:Float = bitmap.height.toFloat()/2) {
+        var rectPixels:LinkedList<Pixel> = LinkedList()
+        var circlePixels:LinkedList<Pixel> = LinkedList()
         fun draw(canvas:Canvas,paint:Paint) {
             if(rectPixels.size == 0) {
                 separatePixels()
             }
             rectPixels.forEach { pixel ->
-                paint.color = pixel
-                canvas.drawRect(0f,0f,1f,1f,paint)
+                pixel.draw(canvas,paint)
             }
+            canvas.save()
+            canvas.translate(cx,cy)
+            canvas.rotate(180f)
+            canvas.save()
+            canvas.translate(-cx,-cy)
+            circlePixels.forEach { pixel ->
+                pixel.draw(canvas,paint)
+            }
+            canvas.restore()
+            canvas.restore()
         }
         private fun separatePixels() {
             for(i in 0..bitmap.width-1) {
                 for(j in 0..bitmap.height-1) {
-                    val pixel = bitmap.getPixel(i,j)
+                    val color = bitmap.getPixel(i,j)
+                    val pixel = Pixel(i.toFloat(),j.toFloat(),color)
                     var distance = getDistance(i.toFloat(),j.toFloat())
                     if(distance>size/2) {
                         rectPixels.add(pixel)
@@ -51,9 +62,13 @@ class ImageCircleClipperView(ctx:Context,var bitmap:Bitmap):View(ctx) {
             }
         }
         private fun getDistance(x:Float,y:Float):Float {
-            val cx = bitmap.width/2
-            val cy = bitmap.height/2
             return Math.sqrt(Math.pow((cx-x).toDouble(),2.0)+Math.pow((cy-y).toDouble(),2.0)).toFloat()
+        }
+    }
+    data class Pixel(var x:Float,var y:Float,var color:Int) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.color = color
+            canvas.drawRect(RectF(x,y,x+1,y+1),paint)
         }
     }
 }
