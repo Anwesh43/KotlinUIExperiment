a6e71e7c3235eeebf07e7e0cfb173e7a33e8b577
Anwesh43 <anweshthecool0@gmail.com>
2017-10-05 01:14:41 +0530

    using ColorPageSwiperView in activity

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/MainActivity.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/MainActivity.kt
@@ -132,7 +132,8 @@ class MainActivity : AppCompatActivity() {
 //        addContentView(textView,ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT))
         //PieColorBarView.create(this,{Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()})
        // RectBarExpanderView.create(this)
-        ImageCircleClipperView.create(this,bitmap,{Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()})
+        //ImageCircleClipperView.create(this,bitmap,{Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()})
+        ColorPageSwiperView.create(this,arrayOf(Color.parseColor("#00BCD4"),Color.parseColor("#F4511E"),Color.parseColor("#f44336"),Color.parseColor("#303F9F"),Color.parseColor("#00838F"),Color.parseColor("#6A1B9A")))
     }
 }
 //data class CompletionListener(var activity: MainActivity):CircleCreatorView.OnCompletionListener {
