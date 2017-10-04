20b19a6971d89a9c44147f21d670c23d7fd3bc6c
Anwesh43 <anweshthecool0@gmail.com>
2017-10-05 00:18:36 +0530

    adding Indicator and ColorPage

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorPageSwiperView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorPageSwiperView.kt
@@ -2,7 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +23,26 @@ class ColorPageSwiperView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class ColorPageContainer(var w:Float,var h:Float) {
+
+    }
+    data class IndicatorContainer(var x:Float,var y:Float,var size:Float,var n:Int,var i:Int = 0) {
+
+    }
+    data class ColorPage(var x:Float,var w:Float,var h:Float,var color:Int) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,0f)
+            paint.color = color
+            canvas.drawRect(RectF(0f,0f,w,h),paint)
+            canvas.restore()
+        }
+    }
+    data class Indicator(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas: Canvas,paint:Paint,scale:Float) {
+            paint.color = Color.WHITE
+            canvas.drawCircle(x,y,size/2+size/2*scale,paint)
+        }
     }
 }
