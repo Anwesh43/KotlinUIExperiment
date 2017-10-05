918085554c3b3955e5986b71b5bc05cdb7ad6b27
Anwesh43 <anweshthecool0@gmail.com>
2017-10-06 01:31:52 +0530

    adding LineCircleState

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiLineCircleView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiLineCircleView.kt
@@ -46,4 +46,21 @@ class MultiLineCircleView(ctx:Context):View(ctx) {
         }
         fun stopped():Boolean = false
     }
+    data class LineCircleState(var scale:Float = 0f,var dir:Float = 0f) {
+        fun update() {
+            scale += dir*0.1f
+            if(scale > 1) {
+                dir = 0f
+                scale = 1f
+            }
+            if(scale < 0) {
+                scale = 0f
+                dir = 0f
+            }
+        }
+        fun startUpdating() {
+            dir = 1-2*scale
+        }
+        fun stopped():Boolean = dir == 0f
+    }
 }
