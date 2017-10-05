312b7cb9ea059424469f9a7bbafe808b3644062e
Anwesh43 <anweshthecool0@gmail.com>
2017-10-06 01:29:03 +0530

    adding LineCircle

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiLineCircleView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiLineCircleView.kt
@@ -3,6 +3,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
 
@@ -21,5 +22,28 @@ class MultiLineCircleView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class LineCircle(var x:Float,var y:Float,var hSize:Float,var rSize:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(0f,0f,rSize/2,paint)
+            canvas.drawArc(RectF(-rSize/2,-rSize/2,rSize/2,rSize/2),0f,360f,true,paint)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.scale(1f,1f-2)
+                canvas.drawLine(0f,0f,0f,hSize/2-rSize/2,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = false
     }
 }
