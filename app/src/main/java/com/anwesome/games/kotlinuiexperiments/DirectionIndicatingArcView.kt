267d70eb2432631e1a4cb8d625d705139ba84298
Anwesh43 <anweshthecool0@gmail.com>
2017-10-08 02:43:04 +0530

    adding DirectionIndicatorArcContainer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirectionIndicatingArcView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirectionIndicatingArcView.kt
@@ -2,6 +2,8 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.Context
 import android.view.*
 import android.graphics.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
 /**
  * Created by anweshmishra on 08/10/17.
  */
@@ -18,16 +20,50 @@ class DirectionIndicatingArcView(ctx:Context):View(ctx) {
         }
         return true
     }
-    data class DirectionIndicatingArc(var x:Float,var y:Float,var r:Float,var deg:Float) {
-        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+    data class DirectionIndicatingArc(var x:Float,var y:Float,var r:Float,var deg:Float,var scale:Float = 0f) {
+        fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(x,y)
             paint.style = Paint.Style.STROKE
             canvas.drawCircle(0f,0f,r,paint)
             paint.style = Paint.Style.FILL
-            canvas.drawArc(RectF(-r,-r,r,r),0f,360f,true,paint)
+            canvas.drawArc(RectF(-r,-r,r,r),0f,360f*scale,true,paint)
             canvas.restore()
         }
         fun handleTap(x:Float,y:Float) = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
+    }
+    data class DirectionIndicatingArcContainer(var w:Float,var h:Float,var arcs:ConcurrentLinkedQueue<DirectionIndicatingArc> = ConcurrentLinkedQueue()) {
+        var prev:DirectionIndicatingArc?=null
+        var curr:DirectionIndicatingArc?=null
+        var gapDeg = 0f
+        init {
+            val size = 2*Math.min(w,h)/5
+            val circlePoint:(Int)->PointF = {i -> PointF(size*Math.cos(i*Math.PI/2).toFloat(),size*Math.sin(i*Math.PI/2).toFloat())}
+            for(i in 0..3) {
+                val point = circlePoint(i)
+                val arc = DirectionIndicatingArc(point.x,point.y,size/5,i*90f)
+                arcs.add(arc)
+                if(i == 0) {
+                    prev = arc
+                }
+            }
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            arcs.forEach { arc ->
+                arc.draw(canvas,paint)
+            }
+        }
+        fun update() {
+        }
+        fun stopped():Boolean = true
+        fun handleTap(x:Float,y:Float,startcb:()->Unit) {
+            arcs.forEach { arc ->
+                if(arc.handleTap(x,y) && arc != prev) {
+                    gapDeg = arc.deg - (prev?.deg?:0f)
+                    curr = arc
+                    startcb()
+                }
+            }
+        }
     }
 }
