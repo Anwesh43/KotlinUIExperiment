63305363e077519f1cafd1591525b511c36a8388
Anwesh43 <anweshthecool0@gmail.com>
2017-10-10 02:05:44 +0530

    adding PointedArrow

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/PointedArrowView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/PointedArrowView.kt
@@ -19,4 +19,37 @@ class PointedArrowView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class PointedArrow(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            val j = Math.floor(3.0).toInt()
+            for(i in 0..3) {
+                canvas.drawDirectedLine(size/2,i*90f,paint)
+            }
+            canvas.drawDirectedLine(size/2,j*90f+90f,paint)
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun stopped():Boolean = false
+        fun startUpdating() {
+
+        }
+    }
+}
+fun Canvas.drawDirectedLine(size:Float,deg:Float,paint:Paint) {
+    this.save()
+    this.rotate(deg)
+    this.drawLine(size,-size/2,size,-size/2,paint)
+    val path = Path()
+    path.addDownTriangle(size,size/2,size/10)
+    this.drawPath(path,paint)
+    this.restore()
+}
+fun Path.addDownTriangle(x:Float,y:Float,size:Float) {
+    this.moveTo(x-size/2,y-size/2)
+    this.lineTo(x+size/2,y-size/2)
+    this.lineTo(x,y+size/2)
 }
