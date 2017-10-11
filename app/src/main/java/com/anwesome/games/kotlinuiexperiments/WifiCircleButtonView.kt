9e70d3c8baad50b03f34950bc6be56d27437abd3
Anwesh43 <anweshthecool0@gmail.com>
2017-10-11 09:07:18 +0530

    using WifiCircleButtonRenderer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/WifiCircleButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/WifiCircleButtonView.kt
@@ -8,13 +8,15 @@ import android.view.*
  */
 class WifiCircleButtonView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = WifiCirlceButtonRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN ->{
-
+                renderer.handleTap()
             }
         }
         return true
