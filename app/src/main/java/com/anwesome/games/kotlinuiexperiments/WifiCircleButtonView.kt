abf21a249b4a0dc0e0ce801b11a568c744f629be
Anwesh43 <anweshthecool0@gmail.com>
2017-10-11 07:59:27 +0530

    adding WifiCircleButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/WifiCircleButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/WifiCircleButtonView.kt
@@ -19,4 +19,49 @@ class WifiCircleButtonView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class WifiCircleButton(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.scale(1f-2*i,1f)
+                for(j in 1..4) {
+                    val newR = r/j
+                    for(k in 0..1) {
+                        canvas.save()
+                        canvas.rotate(-45+45f*(1-2*k))
+                        canvas.drawPointArc(0f,0f,newR,0,90,paint)
+                        canvas.restore()
+                    }
+                }
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun startUpdating() {
+
+        }
+        fun update() {
+
+        }
+        fun stopped():Boolean = false
+    }
+}
+fun Canvas.drawPointArc(x:Float,y:Float,r:Float,start:Int,end:Int,paint:Paint) {
+    val path = Path()
+    path.moveInCircle(x,y,r,start,end)
+    this.drawPath(path,paint)
+}
+fun Path.moveInCircle(cx:Float,cy:Float,r:Float,start:Int,end:Int) {
+    for(i in start..end) {
+        val x = cx+r*Math.cos(i*Math.PI/180).toFloat()
+        val y = cx+r*Math.sin(i*Math.PI/180).toFloat()
+        if(i == start) {
+            this.moveTo(x,y)
+        }
+        else {
+            this.lineTo(x,y)
+        }
+    }
 }
