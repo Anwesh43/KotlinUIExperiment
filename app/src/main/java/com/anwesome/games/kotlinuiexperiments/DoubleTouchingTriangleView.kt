6ddb51a4b6e87a719cf00cf3eac0abb829de91d0
Anwesh43 <anweshthecool0@gmail.com>
2017-10-12 09:55:23 +0530

    adding DoubleTouchTriangle

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DoubleTouchingTriangleView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DoubleTouchingTriangleView.kt
@@ -19,4 +19,38 @@ class DoubleTouchingTriangleView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class DoubleTouchingTriangle(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.scale(1f-2*i,1f)
+                canvas.save()
+                canvas.drawRotatedHorizontalTriangle(size/2,0f,180f,size,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
+    }
+}
+fun Path.addHorizontalTriangle(size:Float) {
+    this.moveTo(-size/2,0f)
+    this.lineTo(size/2,-size/2)
+    this.lineTo(size/2,size/2)
+}
+fun Canvas.drawRotatedHorizontalTriangle(x:Float,y:Float,deg:Float,size:Float,paint:Paint) {
+    this.save()
+    this.rotate(deg)
+    val path = Path()
+    path.addHorizontalTriangle(size)
+    this.drawPath(path,paint)
+    this.restore()
 }
