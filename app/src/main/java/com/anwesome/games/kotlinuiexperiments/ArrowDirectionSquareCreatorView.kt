ef0e829375d5b5877e848c4d77ced54d3b8257ad
Anwesh43 <anweshthecool0@gmail.com>
2017-10-13 09:07:36 +0530

    adding ArrowDirectionSquareCreator

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArrowDirectionSquareCreatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArrowDirectionSquareCreatorView.kt
@@ -17,4 +17,29 @@ class ArrowDirectionSquareCreatorView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class ArrowDirectionSquareCreator(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            for(i in 0..4) {
+                canvas.save()
+                canvas.rotate(-90f*i)
+                canvas.drawLine(-size/2,-size/2,size/2,-size/2,paint)
+                canvas.restore()
+            }
+            canvas.save()
+            canvas.rotate(90f+90f)
+            canvas.drawLine(-size/2,-size/2,-size/2+size,-size/2,paint)
+            canvas.drawRotatedHorizontalTriangle(-size/2,-size/2+size,0f,size/25,paint)
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
+    }
 }
