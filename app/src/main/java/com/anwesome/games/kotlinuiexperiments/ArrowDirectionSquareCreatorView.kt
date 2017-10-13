44fcac89e27bd9ced513a2fd838197232c360ad6
Anwesh43 <anweshthecool0@gmail.com>
2017-10-13 10:21:02 +0530

    adding static create method

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArrowDirectionSquareCreatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArrowDirectionSquareCreatorView.kt
@@ -1,4 +1,5 @@
 package com.anwesome.games.kotlinuiexperiments
+import android.app.Activity
 import android.view.*
 import android.content.*
 import android.graphics.*
@@ -110,6 +111,13 @@ class ArrowDirectionSquareCreatorView(ctx:Context):View(ctx) {
         }
         fun handleTap() {
             animator?.startUpdating()
+        }
+        companion object{
+            fun create(activity:Activity) {
+                val view = ArrowDirectionSquareCreatorView(activity)
+                val size = DimensionsUtil.getDimension(activity)
+                activity.addContentView(view,ViewGroup.LayoutParams(size.x/2,size.x/2))
+            }
         }
     }
 }
