ae3f17bf88df0338d0f11764acbc754928960fd9
Anwesh43 <anweshthecool0@gmail.com>
2017-10-14 09:14:59 +0530

    adding CornerBall

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CenterToCornerBallView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CenterToCornerBallView.kt
@@ -18,4 +18,19 @@ class CenterToCornerBallView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class CornerBall(var i:Float,var w:Float,var h:Float,var x:Float = 0f,var y:Float = 0f,var r:Float = Math.min(w,h)/20) {
+        init {
+            x = (i%2)*(w-2*r)+r
+            y = (i/2)*(h-2*r)+r
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.strokeWidth = r/10
+            paint.color = Color.parseColor("#FF9800")
+            canvas.save()
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(x,y,r,paint)
+            canvas.restore()
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
+    }
 }
