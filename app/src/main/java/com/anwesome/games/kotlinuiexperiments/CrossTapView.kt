4dcfb77813a7efb4fcaf878f4f59a50727d6ffd0
Anwesh43 <anweshthecool0@gmail.com>
2017-10-15 09:21:35 +0530

    adding GameRunner

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossTapView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossTapView.kt
@@ -23,9 +23,28 @@ class CrossTapView(context:Context):SurfaceView(context) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    class GameRunner(var animated:Boolean = true,var view:CrossTapView):Runnable {
+        override fun run() {
+            while(animated) {
+                view.render()
+                try {
+                    Thread.sleep(75)
+                }
+                catch(ex:Exception) {
+
+                }
+            }
+        }
+        fun pause() {
+            animated = false
+        }
+        fun resume() {
+            animated = true
+        }
     }
 }
