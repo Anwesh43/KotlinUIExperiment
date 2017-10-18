d8129727252af1fb16580d6c9f6d4e6d18cb3a7d
Anwesh43 <anweshthecool0@gmail.com>
2017-10-18 11:02:23 +0530

    adding LinkedBallButtonView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/LinkedBallButtonView.kt
@@ -0,0 +1,22 @@
+package com.anwesome.games.kotlinuiexperiments
+import android.view.*
+import android.content.*
+import android.graphics.*
+/**
+ * Created by anweshmishra on 18/10/17.
+ */
+
+class LinkedBallButtonView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
