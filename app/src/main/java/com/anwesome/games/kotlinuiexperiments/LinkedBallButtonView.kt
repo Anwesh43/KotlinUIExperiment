107bd33251d89647397aea66968fe3a06ab86cc2
Anwesh43 <anweshthecool0@gmail.com>
2017-10-18 14:33:17 +0530

    adding LinkedBallButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/LinkedBallButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/LinkedBallButtonView.kt
@@ -2,6 +2,9 @@ package com.anwesome.games.kotlinuiexperiments
 import android.view.*
 import android.content.*
 import android.graphics.*
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
 /**
  * Created by anweshmishra on 18/10/17.
  */
@@ -56,6 +59,43 @@ class LinkedBallButtonView(ctx:Context):View(ctx) {
         fun stopped():Boolean = dir==0f
         fun startUpdating() {
             dir = 1-2*this.scale
+        }
+    }
+    data class LinkedBallButton(var w:Float,var h:Float) {
+        var ballButtons:ConcurrentLinkedQueue<BallButton> = ConcurrentLinkedQueue()
+        var updatingBalls:LinkedList<BallButton> = LinkedList()
+        init {
+            val n = 6
+            if(w>h) {
+                val r = h / 4
+                val size = (w - n * r) / n
+                var x = r
+                for (i in 1..n) {
+                    ballButtons.add(BallButton(x,h/2,r,size))
+                    x += 2*r+size
+                }
+            }
+        }
+        fun update(stopcb:()->Unit) {
+            updatingBalls.forEach { ball ->
+                ball.update()
+                if(ball.stopped()) {
+                    updatingBalls.remove(ball)
+                }
+            }
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            ballButtons.forEach{ ballButton ->
+                ballButton.draw(canvas,paint)
+            }
+        }
+        fun handleTap(x:Float,y:Float,startcb:()->Unit) {
+            ballButtons.forEach { ballButton ->
+                if(ballButton.handleTap(x,y)) {
+                    updatingBalls.add(ballButton)
+                    startcb()
+                }
+            }
         }
     }
 }
