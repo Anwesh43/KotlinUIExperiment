0c3ee8ecf5c1c396e04665fc8467b21aee767209
Anwesh43 <anweshthecool0@gmail.com>
2017-10-20 04:35:58 +0530

    adding arrowtiprotator

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArrowTipRotatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArrowTipRotatorView.kt
@@ -18,4 +18,29 @@ class ArrowTipRotatorView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class ArrowTipRotator(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.strokeWidth = size/12
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawLine(0f,0f,0f,-size,paint)
+            canvas.save()
+            canvas.translate(0f,-size)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.rotate((2*i-1)*45f)
+                canvas.drawLine(0f,0f,0f,size/4,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
+    }
 }
