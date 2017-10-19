dc3063858ab5f9003a3b7109f9692476653deed2
Anwesh43 <anweshthecool0@gmail.com>
2017-10-20 05:06:54 +0530

    using renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArrowTipRotatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArrowTipRotatorView.kt
@@ -7,13 +7,14 @@ import android.graphics.*
  */
 class ArrowTipRotatorView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = ArrowTipRenderer(view=this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN ->{
-
+                renderer.handleTap()
             }
         }
         return true
