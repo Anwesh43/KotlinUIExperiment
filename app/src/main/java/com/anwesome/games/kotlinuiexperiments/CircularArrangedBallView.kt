cf08b6480c64c883cdbad12e7ffc8373bfaa7947
Anwesh43 <anweshthecool0@gmail.com>
2017-10-19 19:52:03 +0530

    adding CircularArrangedBallContainer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircularArrangedBallView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircularArrangedBallView.kt
@@ -3,6 +3,8 @@ package com.anwesome.games.kotlinuiexperiments
 import android.content.*
 import android.view.*
 import android.graphics.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
 /**
  * Created by anweshmishra on 19/10/17.
  */
@@ -56,5 +58,47 @@ class CircularArrangedBallView(ctx:Context):View(ctx) {
             dir = 1-2*scale
         }
         fun stopped():Boolean = dir == 0f
+    }
+    data class CircularArrangedBallContainer(var w:Float,var h:Float,var n:Int) {
+        var balls:ConcurrentLinkedQueue<CircularArrangedBall> = ConcurrentLinkedQueue()
+        var updatingBalls:ConcurrentLinkedQueue<CircularArrangedBall> = ConcurrentLinkedQueue()
+        init {
+            if(n > 0) {
+                val deg = 360/n
+                var currDeg = deg
+                val r = Math.min(w,h)/5
+                val ballR = Math.min(w,h)/20
+                for (i in 1..n) {
+                    balls.add(CircularArrangedBall(r*Math.cos(currDeg*Math.PI/180).toInt(),r*Math.sin(currDeg*Math.PI/180).toInt(),ballR))
+                    currDeg += deg
+                }
+            }
+        }
+        fun update(stopCb:()->Unit) {
+            updatingBalls.forEach { ball ->
+                ball.update()
+                if(ball.stopped()) {
+                    updatingBalls.remove(ball)
+                    if(updatingBalls.size == 0) {
+                        stopCb()
+                    }
+                }
+            }
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            balls.forEach { ball ->
+                ball.draw(canvas,paint)
+            }
+        }
+        fun handleTap(x:Float,y:Float,startCb:()->Unit) {
+            balls.forEach{ ball ->
+                if(ball.handleTap(x-w/2,y-h/2)) {
+                    updatingBalls.add(ball)
+                    if(updatingBalls.size == 0) {
+                        startCb()
+                    }
+                }
+            }
+        }
     }
 }
