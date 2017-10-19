cc38350b065722bffcf1e100a49798629b50c85a
Anwesh43 <anweshthecool0@gmail.com>
2017-10-19 20:16:43 +0530

    using renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircularArrangedBallView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircularArrangedBallView.kt
@@ -9,14 +9,16 @@ import java.util.concurrent.ConcurrentLinkedQueue
  * Created by anweshmishra on 19/10/17.
  */
 class CircularArrangedBallView(ctx:Context,var n:Int = 6):View(ctx) {
+    val renderer = CircularArrangedBallRenderer(view = this)
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean{
         when(event.action) {
             MotionEvent.ACTION_DOWN ->{
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
