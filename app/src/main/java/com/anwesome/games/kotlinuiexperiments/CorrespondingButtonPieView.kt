14e1b02d36180c3bf9e8a5fad09bee3347177a69
Anwesh43 <anweshthecool0@gmail.com>
2017-10-22 00:58:55 +0530

    adding CorrespondingButtonPie

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CorrespondingButtonPieView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CorrespondingButtonPieView.kt
@@ -19,4 +19,30 @@ class CorrespondingButtonPieView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class CorrespondingButtonPie(var i:Int,var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint,px:Float,py:Float,pr:Float,gap:Float) {
+            paint.style = Paint.Style.FILL
+            canvas.save()
+            canvas.translate(px,py)
+            canvas.drawArc(RectF(-pr,-pr,pr,pr),i*gap,60f,true,paint)
+            canvas.restore()
+            paint.style = Paint.Style.STROKE
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawCircle(0f,0f,r,paint)
+            paint.style = Paint.Style.FILL
+            canvas.save()
+            canvas.scale(1f,1f)
+            canvas.drawCircle(0f,0f,r,paint)
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
+    }
 }
