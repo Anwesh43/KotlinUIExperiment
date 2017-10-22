65e6092effff54b8160f703e4c7dbfc9e00244cb
Anwesh43 <anweshthecool0@gmail.com>
2017-10-23 01:08:49 +0530

    using state in rotatelinebutton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateLineButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateLineButtonView.kt
@@ -6,6 +6,8 @@ package com.anwesome.games.kotlinuiexperiments
 import android.view.*
 import android.content.*
 import android.graphics.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
 class RotateLineButtonView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
@@ -20,28 +22,30 @@ class RotateLineButtonView(ctx:Context):View(ctx) {
         return true
     }
     data class RotateLineButton(var i:Int,var x:Float,var y:Float,var r:Float,var size:Float) {
+        var state = RotateLineButtonState()
         fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(x,y)
             paint.style = Paint.Style.STROKE
             canvas.drawCircle(0f,0f,r,paint)
             canvas.save()
-            canvas.scale(1f,1f)
+            canvas.scale(state.scale,state.scale)
             canvas.drawCircle(0f,0f,r,paint)
             canvas.restore()
             canvas.save()
-            canvas.rotate(90f*(1-2*i))
+            canvas.rotate(90f*(1-2*i)*state.scale)
             canvas.drawLine(0f,-r,0f,-r-size,paint)
             canvas.restore()
             canvas.restore()
         }
         fun update() {
-
+            state.update()
         }
         fun startUpdating() {
-
+            state.startUpdating()
         }
-        fun stopped():Boolean = true
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r && x<=this.x+r && y>=this.y-r && y<=this.y+r
+        fun stopped():Boolean = state.stopped()
     }
     data class RotateLineButtonState(var scale:Float = 0f,var dir:Float = 0f) {
         fun update() {
@@ -59,6 +63,16 @@ class RotateLineButtonView(ctx:Context):View(ctx) {
 
         fun startUpdating() {
             dir = 1-2*this.scale
+        }
+    }
+    data class RotateLineButtonContainer(var w:Float,var h:Float) {
+        var  rotatingLines:ConcurrentLinkedQueue<RotateLineButton> = ConcurrentLinkedQueue()
+        var updatingLines:ConcurrentLinkedQueue<RotateLineButton> = ConcurrentLinkedQueue()
+        fun update() {
+
+        }
+        fun handleTap(x:Float,y:Float) {
+
         }
     }
 }
