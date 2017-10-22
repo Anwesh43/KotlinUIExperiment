41e0f9201fa8268602b211a5a764c28378ac0ed1
Anwesh43 <anweshthecool0@gmail.com>
2017-10-23 00:59:19 +0530

    adding RotatingLineButton

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateLineButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateLineButtonView.kt
@@ -14,9 +14,33 @@ class RotateLineButtonView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    data class RotateLineButton(var i:Int,var x:Float,var y:Float,var r:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(0f,0f,r,paint)
+            canvas.save()
+            canvas.scale(1f,1f)
+            canvas.drawCircle(0f,0f,r,paint)
+            canvas.restore()
+            canvas.save()
+            canvas.rotate(90f*(1-2*i))
+            canvas.drawLine(0f,-r,0f,-r-size,paint)
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
     }
 }
