87ab8cfd18dbe58de1c9246527e980ed115e1596
Anwesh43 <anweshthecool0@gmail.com>
2017-10-23 01:31:05 +0530

    using renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateLineButtonView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateLineButtonView.kt
@@ -10,13 +10,14 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 class RotateLineButtonView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = RLBRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(x,y)
             }
         }
         return true
