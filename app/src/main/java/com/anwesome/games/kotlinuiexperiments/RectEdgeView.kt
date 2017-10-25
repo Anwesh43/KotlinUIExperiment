f99d93a2f0067378b394413fe24e9625f78a3a2e
Anwesh43 <anweshthecool0@gmail.com>
2017-10-25 10:25:10 +0530

    using renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeView.kt
@@ -9,13 +9,14 @@ import java.util.concurrent.ConcurrentLinkedQueue
  */
 class RectEdgeView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = RectEdgeRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN ->{
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
