a6cf0f3d729c0f272e29c758ee6a54a048881f76
Anwesh43 <anweshthecool0@gmail.com>
2017-10-25 10:02:14 +0530

    using State in RectEdge

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeView.kt
@@ -19,6 +19,7 @@ class RectEdgeView(ctx:Context):View(ctx) {
         return true
     }
     data class RectEdge(var i:Int,var r:Float,var x:Float = r*Math.cos(i*Math.PI/2+Math.PI/4).toFloat() ,var y:Float = r*Math.sin(i*Math.PI/2+Math.PI/4).toFloat()) {
+        var state = RectEdgeState()
         fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(x,y)
@@ -26,21 +27,21 @@ class RectEdgeView(ctx:Context):View(ctx) {
             canvas.drawCircle(0f,0f,r/10,paint)
             paint.style = Paint.Style.FILL
             canvas.save()
-            canvas.drawCircle(0f,0f,r/10,paint)
+            canvas.drawCircle(0f,0f,(r/10)*state.scale,paint)
             canvas.restore()
             canvas.save()
-            canvas.rotate(90f*i+90)
+            canvas.rotate(90f*i+90*state.scale)
             canvas.drawLine(0f,0f,0f,r/Math.sqrt(2.0).toFloat(),paint)
             canvas.restore()
             canvas.restore()
         }
         fun update() {
-
+            state.update()
         }
         fun startUpdating(){
-
+            state.startUpdating()
         }
-        fun stopped():Boolean = true
+        fun stopped():Boolean = state.stopped()
         fun handleTap(x:Float,y:Float):Boolean = x >= this.x - r/10 && x<=this.x+r/10 && y>=this.y-r/10 && y<=this.y+r/10
     }
     data class RectEdgeState(var scale:Float = 0f,var dir:Float = 0f) {
