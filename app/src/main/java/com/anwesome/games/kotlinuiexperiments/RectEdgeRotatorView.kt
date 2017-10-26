e5d445c916085a9952c623828d54898eb6932e4b
Anwesh43 <anweshthecool0@gmail.com>
2017-10-26 08:56:07 +0530

    adding RectEdgeRotator

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeRotatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeRotatorView.kt
@@ -19,4 +19,28 @@ class RectEdgeRotatorView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class RectEdgeRotator(var x:Float,var y:Float,var size:Float){
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.save()
+            canvas.rotate(-30f)
+            canvas.drawLine(0f,0f,size,0f,paint)
+            canvas.restore()
+            for(i in 0..1) {
+                canvas.save()
+                canvas.rotate(-30f*i)
+                canvas.drawCircle(0f,size,size/6,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
+    }
 }
