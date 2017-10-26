55cea5c61d3f3e3b142a9d0e0093a7e81e7fa0c5
Anwesh43 <anweshthecool0@gmail.com>
2017-10-26 09:24:31 +0530

    using RectEdgeRotatorState

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeRotatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeRotatorView.kt
@@ -20,28 +20,29 @@ class RectEdgeRotatorView(ctx:Context):View(ctx) {
         return true
     }
     data class RectEdgeRotator(var x:Float,var y:Float,var size:Float){
+        var state = RectEdgeRotatorState()
         fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(x,y)
             canvas.save()
-            canvas.rotate(-30f)
-            canvas.drawLine(0f,0f,size,0f,paint)
+            canvas.rotate(-30f*state.scales[2])
+            canvas.drawLine(0f,0f,size*state.scales[0],0f,paint)
             canvas.restore()
             for(i in 0..1) {
                 canvas.save()
                 canvas.rotate(-30f*i)
-                canvas.drawCircle(0f,size,size/6,paint)
+                canvas.drawCircle(0f,size,(size/6)*state.scales[1],paint)
                 canvas.restore()
             }
             canvas.restore()
         }
         fun update() {
-
+            state.update()
         }
         fun startUpdating() {
-
+            state.startUpdating()
         }
-        fun stopped():Boolean = true
+        fun stopped():Boolean = state.stopped()
     }
     data class RectEdgeRotatorState(var i:Int = 0,var dir:Int = 0,var currDir:Int = 1) {
         var scales:Array<Float> = arrayOf(0f,0f,0f)
