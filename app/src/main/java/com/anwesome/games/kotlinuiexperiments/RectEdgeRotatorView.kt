28e150b060bdc5207426d3ce78357b0ebfde2a6e
Anwesh43 <anweshthecool0@gmail.com>
2017-10-26 09:38:41 +0530

    using RERRenderer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeRotatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectEdgeRotatorView.kt
@@ -8,13 +8,14 @@ import android.content.*
  */
 class RectEdgeRotatorView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = RERRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
