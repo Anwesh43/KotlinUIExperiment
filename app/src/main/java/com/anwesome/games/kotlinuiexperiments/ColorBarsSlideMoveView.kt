727e6fcb9a0fa231f1a55b0b29b5357dc8e46320
Anwesh43 <anweshthecool0@gmail.com>
2017-10-27 22:22:55 +0530

    using renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorBarsSlideMoveView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ColorBarsSlideMoveView.kt
@@ -9,14 +9,16 @@ import java.util.concurrent.ConcurrentLinkedQueue
  */
 val slideColors:Array<String> = arrayOf("#009688","#FF6F00","#0277BD","#f44336","#283593")
 class ColorBarSlideMoveView(ctx:Context):View(ctx) {
+    val renderer = ColorBarSlideMoveRenderer(this)
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
