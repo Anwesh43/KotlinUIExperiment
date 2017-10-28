ad7abf15e281116e9bed89a4afc62d349fdfdb6d
Anwesh43 <anweshthecool0@gmail.com>
2017-10-28 10:50:00 +0530

    using JointRenderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/LineJoinerView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/LineJoinerView.kt
@@ -10,13 +10,15 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 class LineJoinerView(ctx:Context,var n:Int = 5):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = JointRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
