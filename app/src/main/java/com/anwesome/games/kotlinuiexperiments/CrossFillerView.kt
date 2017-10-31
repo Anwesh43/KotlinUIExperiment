e6cf0ac43a96698aeb10a162da2cf9e1da80f8a7
Anwesh43 <anweshthecool0@gmail.com>
2017-10-31 20:16:29 +0530

    using renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossFillerView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossFillerView.kt
@@ -8,13 +8,15 @@ import android.content.*
 import android.graphics.*
 class CrossFillerView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = CrossFillerRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
