1fc248b292c6824fdb67fa02d2f7a5198cf09630
Anwesh43 <anweshthecool0@gmail.com>
2017-10-31 10:45:51 +0530

    using state in crossfiller

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossFillerView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossFillerView.kt
@@ -20,10 +20,11 @@ class CrossFillerView(ctx:Context):View(ctx) {
         return true
     }
     data class CrossFiller(var x:Float,var y:Float,var size:Float) {
+        var state = CrossFillerState()
         fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(x,y)
-            canvas.rotate(45f)
+            canvas.rotate(45f*state.scales[1])
             paint.strokeWidth = size/30
             paint.strokeCap = Paint.Cap.ROUND
             for(i in 0..3) {
@@ -32,18 +33,18 @@ class CrossFillerView(ctx:Context):View(ctx) {
                 paint.color = Color.GRAY
                 canvas.drawLine(0f,0f,0f,-size/2,paint)
                 paint.color = Color.BLUE
-                canvas.drawLine(0f,-size/2+size/2,0f,-size/2,paint)
+                canvas.drawLine(0f,-size/2+size/2*state.scales[0],0f,-size/2,paint)
                 canvas.restore()
             }
             canvas.restore()
         }
         fun update() {
-
+            state.update()
         }
         fun startUpdating() {
-
+            state.startUpdating()
         }
-        fun stopped():Boolean = true
+        fun stopped():Boolean = state.stopped()
     }
     data class CrossFillerState(var dir:Int = 0,var j:Int = 0,var prevScale:Float = 0f,var currDir:Int = 1) {
         var scales:Array<Float> = arrayOf(0f,0f)
