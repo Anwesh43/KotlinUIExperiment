3df4a4de2ee7925ef330fc744e283f84f1becbd8
Anwesh43 <anweshthecool0@gmail.com>
2017-10-31 10:38:03 +0530

    adding CrossFiller

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossFillerView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossFillerView.kt
@@ -19,4 +19,30 @@ class CrossFillerView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class CrossFiller(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(45f)
+            paint.strokeWidth = size/30
+            paint.strokeCap = Paint.Cap.ROUND
+            for(i in 0..3) {
+                canvas.save()
+                canvas.rotate(i*90f)
+                paint.color = Color.GRAY
+                canvas.drawLine(0f,0f,0f,-size/2,paint)
+                paint.color = Color.BLUE
+                canvas.drawLine(0f,-size/2+size/2,0f,-size/2,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
+    }
 }
