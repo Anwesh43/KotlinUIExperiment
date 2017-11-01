8ddaaded98fd692b7f253c56dfb535b1d81cb355
Anwesh43 <anweshthecool0@gmail.com>
2017-11-01 09:46:54 +0530

    using Renderer in BarOverView

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/BarOverView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/BarOverView.kt
@@ -7,13 +7,15 @@ import android.graphics.*
  */
 class BarOverView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = BarOverRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
