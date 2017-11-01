76702a12e473a574f9f3c31704299418b123593d
Anwesh43 <anweshthecool0@gmail.com>
2017-11-01 09:28:29 +0530

    using BarOverState

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/BarOverView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/BarOverView.kt
@@ -19,19 +19,20 @@ class BarOverView(ctx:Context):View(ctx) {
         return true
     }
     data class BarOver(var x:Float,var y:Float,var w:Float,var h:Float) {
+        val state = BarOverState()
         fun draw(canvas:Canvas,paint:Paint) {
             for(i in 0..1) {
                 canvas.save()
-                canvas.translate(x+(w/2*(i*2-1)),y)
+                canvas.translate(x+(w/2*(i*2-1))*state.scale,y)
                 paint.color = Color.parseColor("#1565C0")
                 canvas.drawRect(RectF(-w/2,-h/2,w/2,h/2),paint)
                 canvas.restore()
             }
         }
         fun update() {
-
+            state.update()
         }
-        fun stopped():Boolean = true
+        fun stopped():Boolean = state.stopped()
     }
     data class BarOverState(var scale:Float = 0f,var deg:Float = 0f) {
         fun update() {
