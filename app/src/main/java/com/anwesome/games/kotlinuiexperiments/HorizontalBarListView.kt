89edc7d1c0ba19f633ac4fe85b9c6539b9f61557
Anwesh43 <anweshthecool0@gmail.com>
2017-11-02 08:25:30 +0530

    using HorizontalBarRenderer in HorizontalBarListView

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/HorizontalBarListView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/HorizontalBarListView.kt
@@ -10,13 +10,15 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 class HorizontalBarListView(ctx:Context,var n:Int = 10):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = HorizontalBarRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
