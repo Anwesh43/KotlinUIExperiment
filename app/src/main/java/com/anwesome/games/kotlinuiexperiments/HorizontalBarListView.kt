7c54ac0ac118f44e875be78cd861068ca3397a4d
Anwesh43 <anweshthecool0@gmail.com>
2017-11-02 08:11:52 +0530

    adding HorizontalBarList

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/HorizontalBarListView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/HorizontalBarListView.kt
@@ -6,6 +6,8 @@ package com.anwesome.games.kotlinuiexperiments
 import android.graphics.*
 import android.content.*
 import android.view.*
+import java.util.concurrent.ConcurrentLinkedQueue
+
 class HorizontalBarListView(ctx:Context,var n:Int = 10):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
@@ -19,7 +21,7 @@ class HorizontalBarListView(ctx:Context,var n:Int = 10):View(ctx) {
         }
         return true
     }
-    data class HorizontalBar(var x:Float,var y:Float,var w:Float,var h:Float) {
+    data class HorizontalBar(var i:Int,var x:Float,var y:Float,var w:Float,var h:Float) {
         fun draw(canvas:Canvas,paint:Paint) {
             paint.strokeCap = Paint.Cap.ROUND
             paint.strokeWidth = Math.min(w,h)/40
@@ -43,5 +45,42 @@ class HorizontalBarListView(ctx:Context,var n:Int = 10):View(ctx) {
         }
         fun stopped():Boolean = true
         fun handleTap(x:Float,y:Float):Boolean = x>=this.x-w/2 && x<=this.x+w/2 && y>=this.y-w/2 && y<=this.y+w/2
+    }
+    data class HorizontalBarList(var w:Float,var h:Float,var n:Int) {
+        var horizontalBars:ConcurrentLinkedQueue<HorizontalBar> = ConcurrentLinkedQueue()
+        var prev:HorizontalBar?=null
+        var curr:HorizontalBar?=null
+        init {
+            if(n > 0) {
+                val wsize = w / n
+                var x = wsize/2
+                val y = wsize/2
+                for (i in 1..n) {
+                    horizontalBars.add(HorizontalBar(i,x,y,wsize,h/2))
+                }
+            }
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            horizontalBars.forEach {
+                it.draw(canvas,paint)
+            }
+        }
+        fun update(stopcb:()->Unit) {
+            prev?.update()
+            curr?.update()
+            if(prev?.stopped()?:true && curr?.stopped()?:false) {
+                prev = curr
+                curr = null
+                stopcb()
+            }
+        }
+        fun handleTap(x:Float,y:Float,startcb:()->Unit) {
+            horizontalBars.forEach { it ->
+                if(it.handleTap(x,y)) {
+                    curr = it
+                    startcb()
+                }
+            }
+        }
     }
 }
