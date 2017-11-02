af1470c4db69d964c014f7786845dcd8a8608379
Anwesh43 <anweshthecool0@gmail.com>
2017-11-02 08:03:32 +0530

    adding HorizontalBar

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/HorizontalBarListView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/HorizontalBarListView.kt
@@ -19,4 +19,29 @@ class HorizontalBarListView(ctx:Context,var n:Int = 10):View(ctx) {
         }
         return true
     }
+    data class HorizontalBar(var x:Float,var y:Float,var w:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.strokeWidth = Math.min(w,h)/40
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(45f)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.rotate(90f*i)
+                canvas.drawLine(0f,-w/3,0f,w/3,paint)
+                canvas.restore()
+            }
+            canvas.drawRect(RectF(-w/2,w/2,w/2,w/2+h),paint)
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-w/2 && x<=this.x+w/2 && y>=this.y-w/2 && y<=this.y+w/2
+    }
 }
