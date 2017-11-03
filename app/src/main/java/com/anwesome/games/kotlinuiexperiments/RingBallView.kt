b9b9d3fc2cf508f54f6555400724f6de15561432
Anwesh43 <anweshthecool0@gmail.com>
2017-11-03 09:14:14 +0530

    correcting format

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RingBallView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RingBallView.kt
@@ -60,41 +60,46 @@ class RingBallView(ctx:Context):View(ctx) {
             dir = 1-2*this.scale
         }
     }
-}
-data class RingForCenterBall(var deg:Float,var cx:Float,var cy:Float,var r:Float,var x:Float = cx.xInCircle(r,deg),var y:Float = cy.yInCircle(r,deg)) {
-    fun draw(canvas:Canvas,paint:Paint) {
-        canvas.drawCircle(x,y,r/15,paint)
-    }
-    fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r/10 && x<=this.x+r/10 && y>=this.y-r/10 && y<=this.y+r/10
-}
-data class RingForCenterBallContainer(var w:Float,var h:Float,var size:Float = Math.min(w,h)/3) {
-    var rings:ConcurrentLinkedQueue<RingForCenterBall> = ConcurrentLinkedQueue()
-    var centerCornerBall = RingBallView.CenterCornerBall(w/2,h/2,size/10,size)
-    init {
-        for(i in 0..3) {
-            rings.add(RingForCenterBall(90*i+45f,w/2,h/2,size))
+    data class RingForCenterBall(var deg:Float,var cx:Float,var cy:Float,var r:Float,var x:Float = cx.xInCircle(r,deg),var y:Float = cy.yInCircle(r,deg)) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.drawCircle(x,y,r/15,paint)
         }
+        fun handleTap(x:Float,y:Float):Boolean = x>=this.x-r/10 && x<=this.x+r/10 && y>=this.y-r/10 && y<=this.y+r/10
     }
-    fun draw(canvas:Canvas,paint:Paint) {
-        paint.style = Paint.Style.STROKE
-        canvas.drawCircle(w/2,h/2,size/10,paint)
-        rings.forEach { ring ->
-            ring.draw(canvas,paint)
-        }
-        paint.style = Paint.Style.FILL
-        centerCornerBall.draw(canvas,paint)
-    }
-    fun update() {
-        centerCornerBall.update()
-    }
-    fun handleTap(x:Float,y:Float) {
-        rings.forEach { ring ->
-            if(ring.handleTap(x,y)) {
-                centerCornerBall.startUpdating(ring.deg)
+    data class RingForCenterBallContainer(var w:Float,var h:Float,var size:Float = Math.min(w,h)/3) {
+        var rings: ConcurrentLinkedQueue<RingForCenterBall> = ConcurrentLinkedQueue()
+        var centerCornerBall = RingBallView.CenterCornerBall(w / 2, h / 2, size / 10, size)
+
+        init {
+            for (i in 0..3) {
+                rings.add(RingForCenterBall(90 * i + 45f, w / 2, h / 2, size))
             }
         }
+
+        fun draw(canvas: Canvas, paint: Paint) {
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(w / 2, h / 2, size / 10, paint)
+            rings.forEach { ring ->
+                ring.draw(canvas, paint)
+            }
+            paint.style = Paint.Style.FILL
+            centerCornerBall.draw(canvas, paint)
+        }
+
+        fun update() {
+            centerCornerBall.update()
+        }
+
+        fun handleTap(x: Float, y: Float) {
+            rings.forEach { ring ->
+                if (ring.handleTap(x, y)) {
+                    centerCornerBall.startUpdating(ring.deg)
+                }
+            }
+        }
+
+        fun stopped(): Boolean = centerCornerBall.stopped()
     }
-    fun stopped():Boolean = centerCornerBall.stopped()
 }
 fun ConcurrentLinkedQueue<Float>.getAt(i:Int):Float? {
     var index = 0
@@ -106,5 +111,6 @@ fun ConcurrentLinkedQueue<Float>.getAt(i:Int):Float? {
     }
     return null
 }
+
 fun Float.xInCircle(r:Float,deg:Float) = this+r*Math.cos(deg*Math.PI/180).toFloat()
 fun Float.yInCircle(r:Float,deg:Float) = this+r*Math.sin(deg*Math.PI/180).toFloat()
