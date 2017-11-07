bd9be4da70039721bd7a80b800df0516217d134e
Anwesh43 <anweshthecool0@gmail.com>
2017-11-07 07:38:12 +0530

    adding AnalogCameraState

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/AnalogCameraLikeView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/AnalogCameraLikeView.kt
@@ -20,6 +20,7 @@ class AnalogCameraLikeView(ctx:Context):View(ctx) {
         return true
     }
     data class AnalogCameraShape(var w:Float,var h:Float) {
+        var state = AnalogCameraState()
         fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(w/2,h/2)
@@ -29,16 +30,17 @@ class AnalogCameraLikeView(ctx:Context):View(ctx) {
             canvas.drawCircle(0f,0f,w/5,paint)
             paint.color = Color.parseColor("#9E9E9E")
             canvas.drawCircle(0f,0f,w/10,paint)
-            canvas.drawRoundRect(RectF(-w/20,-w/3-w/20,w/20,-w/3),w/30,w/30,paint)
+            val h = -(w/20*state.scale)
+            canvas.drawRoundRect(RectF(-w/20,-w/3-w/20-h,w/20,-w/3-h),w/30,w/30,paint)
             canvas.restore()
         }
         fun update() {
-
+            state.update()
         }
         fun startUpdating() {
-
+            state.startUpdating()
         }
-        fun stopped():Boolean = false
+        fun stopped():Boolean = state.stopped()
         fun handleTap(x:Float,y:Float):Boolean = x>=w/2-w/5 && x<=w/2+w/5 && y>=h/2-w/5 && y<=h/2+w/5
     }
     data class AnalogCameraState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f){
