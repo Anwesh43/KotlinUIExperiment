6c880bab4f625fc94a1f2a06ef3cdb71272b4219
Anwesh43 <anweshthecool0@gmail.com>
2017-11-08 12:03:11 +0530

    adding FourMarkCircle

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourMarkCircleView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourMarkCircleView.kt
@@ -15,9 +15,39 @@ class FourMarkCircleView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    data class FourMarkCircle(var w:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            canvas.drawCircle(0f,0f,w/3,paint)
+            canvas.rotate(90f)
+            for(i in 0..3) {
+                canvas.save()
+                canvas.rotate(90f*i)
+                canvas.save()
+                canvas.translate(0f,-w/4)
+                for(j in 0..1) {
+                    canvas.save()
+                    canvas.rotate(15f*(2*i-1))
+                    canvas.drawLine(0f,-w/4,0f,-w/3,paint)
+                    canvas.restore()
+                }
+                canvas.restore()
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
     }
 }
