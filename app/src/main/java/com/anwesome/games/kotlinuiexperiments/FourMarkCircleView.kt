62e2e5efd800a7563562b5083a0cee84a483c7ab
Anwesh43 <anweshthecool0@gmail.com>
2017-11-08 19:59:03 +0530

    using renderer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourMarkCircleView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourMarkCircleView.kt
@@ -9,13 +9,15 @@ import android.graphics.*
 
 class FourMarkCircleView(ctx:Context):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = FourMarkCircleRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
