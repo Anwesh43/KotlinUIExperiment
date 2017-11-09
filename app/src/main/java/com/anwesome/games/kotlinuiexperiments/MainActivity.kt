e4ea23173c3e4bae87e247266fab6385703d088c
Anwesh43 <anweshthecool0@gmail.com>
2017-11-09 10:22:06 +0530

    using SelectionListener in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/MainActivity.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/MainActivity.kt
@@ -201,7 +201,10 @@ class MainActivity : AppCompatActivity() {
 //        AnalogCameraLikeView.create(this)
 //        var view = FourMarkCircleView.create(this)
 //        view.addClickListener { createToast("clicked") }
-        RotatorDotLineView.create(this)
+        val view = RotatorDotLineView.create(this)
+        view.addSelectionListener { i ->
+            createToast("selected ${i} dot")
+        }
     }
 
     fun createTextView(text:String) {
