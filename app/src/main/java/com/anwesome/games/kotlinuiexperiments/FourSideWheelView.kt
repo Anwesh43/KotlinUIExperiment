9a834d319e144f38d5a4ecd1a81a8c6c607ce2b1
Anwesh43 <anweshthecool0@gmail.com>
2017-11-10 09:58:48 +0530

    adding some further fixes

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourSideWheelView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourSideWheelView.kt
@@ -25,7 +25,9 @@ class FourSideWheelView(ctx:Context):View(ctx) {
     data class FourSideWheel(var w:Float,var h:Float) {
         var state = FourSideWheelState()
         fun draw(canvas:Canvas,paint:Paint) {
-            paint.color = Color.parseColor("#FFC107")
+            paint.strokeWidth = Math.min(w,h)/60
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.color = Color.parseColor("#1565C0")
             val r = Math.min(w,h)/3
             val r1 = 0.4f*Math.min(w,h)
             canvas.save()
@@ -35,10 +37,12 @@ class FourSideWheelView(ctx:Context):View(ctx) {
             canvas.drawCircle(0f,0f,r,paint)
             paint.style = Paint.Style.FILL
             canvas.drawCircle(0f,0f,r*state.scale,paint)
-            for(i in 0..3) {
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = Math.min(w,h)/40
+            for(i in 0..5) {
                 canvas.save()
-                canvas.rotate(90f*i)
-                canvas.drawArc(RectF(-r1,-r1,r1,r1),15f,60f,true,paint)
+                canvas.rotate(60f*i)
+                canvas.drawArc(RectF(-r1,-r1,r1,r1),15f,20f,false,paint)
                 canvas.restore()
             }
             canvas.restore()
@@ -53,9 +57,9 @@ class FourSideWheelView(ctx:Context):View(ctx) {
     }
     data class FourSideWheelState(var dir:Float = 0f,var deg:Float = 0f,var prevDeg:Float = 0f,var scale:Float = 0f) {
         fun update() {
-            deg = prevDeg + 90f*0.1f*dir
-            scale += Math.sin(2*(deg-prevDeg)*Math.PI/180).toFloat()
-            if(prevDeg - deg >= 90) {
+            deg += 90f*0.1f*dir
+            scale = Math.sin(2*(deg-prevDeg)*Math.PI/180).toFloat()
+            if(Math.abs(prevDeg - deg) >= 90) {
                 deg = prevDeg +90f
                 prevDeg = deg
                 dir = 0f
