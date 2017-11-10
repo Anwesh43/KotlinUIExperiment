d91cc1598270cf1de82ad5e6968005afb3f7a916
Anwesh43 <anweshthecool0@gmail.com>
2017-11-10 08:49:23 +0530

    adding FourSideWheelRenderer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourSideWheelView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourSideWheelView.kt
@@ -8,13 +8,15 @@ import android.graphics.*
 import android.view.*
 class FourSideWheelView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = FourSideWheelRenderer(view = this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN ->{
-
+                renderer.handleTap()
             }
         }
         return true
