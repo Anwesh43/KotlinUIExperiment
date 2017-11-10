a3d2f6c57b54632b59f9d0f3248319796de48972
Anwesh43 <anweshthecool0@gmail.com>
2017-11-10 08:39:54 +0530

    using state in FourSideWheel

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourSideWheelView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/FourSideWheelView.kt
@@ -20,15 +20,17 @@ class FourSideWheelView(ctx:Context):View(ctx) {
         return true
     }
     data class FourSideWheel(var w:Float,var h:Float) {
+        var state = FourSideWheelState()
         fun draw(canvas:Canvas,paint:Paint) {
             val r = Math.min(w,h)/3
             val r1 = 0.4f*Math.min(w,h)
             canvas.save()
             canvas.translate(w/2,h/2)
+            canvas.rotate(state.deg)
             paint.style = Paint.Style.STROKE
             canvas.drawCircle(0f,0f,r,paint)
             paint.style = Paint.Style.FILL
-            canvas.drawCircle(0f,0f,r,paint)
+            canvas.drawCircle(0f,0f,r*state.scale,paint)
             for(i in 0..3) {
                 canvas.save()
                 canvas.rotate(90f*i)
@@ -38,17 +40,17 @@ class FourSideWheelView(ctx:Context):View(ctx) {
             canvas.restore()
         }
         fun update() {
-
+            state.update()
         }
         fun startUpdating() {
-
+            state.startUpdating()
         }
-        fun stopped():Boolean = true
+        fun stopped():Boolean = state.stopped()
     }
     data class FourSideWheelState(var dir:Float = 0f,var deg:Float = 0f,var prevDeg:Float = 0f,var scale:Float = 0f) {
         fun update() {
-            scale += Math.sin(2*(deg-prevDeg)*Math.PI/180).toFloat()
             deg = prevDeg + 90f*0.1f*dir
+            scale += Math.sin(2*(deg-prevDeg)*Math.PI/180).toFloat()
             if(prevDeg - deg >= 90) {
                 deg = prevDeg +90f
                 prevDeg = deg
