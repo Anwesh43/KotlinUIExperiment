c3d35757b4a66a90ea2330c1addcada3e9c957ca
Anwesh43 <anweshthecool0@gmail.com>
2017-11-11 10:10:47 +0530

    adding SideExpander

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DoubleSideExpanderView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DoubleSideExpanderView.kt
@@ -15,9 +15,32 @@ class DoubleSideExpanderView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    data class SideExpander(var i:Int,var w:Float,var h:Float,var x:Float=w/2-(w/4+w/10)*(2*i-1),var y:Float=h/2) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(0f,0f,w/10,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-w/10,-w/10,w/10,w/10),0f,360f,true,paint)
+            canvas.restore()
+            val rx = w/2-w/4+i*(w/4)
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            canvas.drawRect(RectF(rx-w/2,-w/8,rx+w/2,w/8),paint)
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
     }
 }
