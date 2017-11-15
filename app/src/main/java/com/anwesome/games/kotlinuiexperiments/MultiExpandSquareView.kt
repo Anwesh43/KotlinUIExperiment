a9a02445904f8390db1019628ad20733bb76a8f9
Anwesh43 <anweshthecool0@gmail.com>
2017-11-15 12:07:53 +0530

    adding MutliExpandSquare

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiExpandSquareView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/MultiExpandSquareView.kt
@@ -15,9 +15,27 @@ class MultiExpandSquareView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean  {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    data class MultiExpandSquare(var i:Int,var n:Int,var x:Float,var y:Float,var size:Float,var cx:Float = x,var cy:Float = y) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            val gap = ((n/2-i)*size/4)
+            x = cx - gap
+            y = cy - gap
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawRect(RectF(-size/2,-size/2,size/2,size/2),paint)
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun startUpdating() {
+
+        }
+        fun stopped():Boolean = true
     }
 }
