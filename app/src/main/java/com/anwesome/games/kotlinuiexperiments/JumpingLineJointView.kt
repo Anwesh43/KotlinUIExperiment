9544db4d1ef0822ec5313a6e8970c5f2fe3baeb2
Anwesh43 <anweshthecool0@gmail.com>
2017-11-20 13:30:09 +0530

    adding jumping linejoint

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/JumpingLineJointView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/JumpingLineJointView.kt
@@ -19,4 +19,18 @@ class JumpingLineJointView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class JumpingLineJoint(var w:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            for(i in 0..1) {
+                canvas.save()
+                canvas.scale(1f,1f-2*i)
+                canvas.drawLine(-w/3,0f,0f,(h/4)*scale,paint)
+                canvas.drawLine(w/3,0f,0f,(h/4)*scale,paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+    }
 }
