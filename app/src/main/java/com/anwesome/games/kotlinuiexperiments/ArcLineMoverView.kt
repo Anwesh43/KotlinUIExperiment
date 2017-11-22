0a6caf27ab7026d974decbb39f2ded13cf7cc4bd
Anwesh43 <anweshthecool0@gmail.com>
2017-11-22 14:54:20 +0530

    adding ArcLineMover

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArcLineMoverView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ArcLineMoverView.kt
@@ -20,4 +20,25 @@ class ArcLineMoverView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class ArcLineMover(var w:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = Math.min(w,h)/40
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            canvas.save()
+            canvas.rotate(90f)
+            canvas.drawLine(0f,0f,w/3,0f,paint)
+            canvas.restore()
+            canvas.drawArc(RectF(-w/3,-w/3,w/3,w/3),0f,90f,false,paint)
+            canvas.restore()
+        }
+        fun update() {
+
+        }
+        fun stopped():Boolean = false
+        fun startUpdating() {
+
+        }
+    }
 }
