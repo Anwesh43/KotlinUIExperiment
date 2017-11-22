9dba4ae75bd80aad95ecba6239b25243b7f0b419
Anwesh43 <anweshthecool0@gmail.com>
2017-11-23 00:48:52 +0530

    adding StepPie

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/StepPieView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/StepPieView.kt
@@ -14,9 +14,18 @@ class StepPieView(ctx:Context):View(ctx) {
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+    data class StepPie(var x:Float,var y:Float,var size:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawArc(RectF(-size/10,-size/10,size/10,size/10),0f,360f*scale,true,paint)
+            canvas.drawLine(0f,0f,size*scale,0f,paint)
+            canvas.restore()
+        }
     }
 }
