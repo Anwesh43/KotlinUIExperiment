d53e686e81f8ae473e3fc367f2310bf60616dd32
Anwesh43 <anweshthecool0@gmail.com>
2017-11-25 10:47:09 +0530

    using Renderer RectArcMoverRenderer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectArcMoverView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectArcMoverView.kt
@@ -9,13 +9,15 @@ import android.graphics.*
 
 class RectArcMoverView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = RectArcMoverRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
