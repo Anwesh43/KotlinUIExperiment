2c6fbd1772d116e64b88af4927148d16725e9206
Anwesh43 <anweshthecool0@gmail.com>
2017-11-25 08:43:58 +0530

    adding RectBarUpDown

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectArcMoverView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RectArcMoverView.kt
@@ -20,4 +20,15 @@ class RectArcMoverView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class RectBarUpDown(var w:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            for(i in 0..1) {
+                canvas.save()
+                canvas.translate(w/2,h/2)
+                canvas.scale(1f,1f-2*i)
+                canvas.drawRect(RectF(-w/2,-h/2,w/2,0f),paint)
+                canvas.restore()
+            }
+        }
+    }
 }
