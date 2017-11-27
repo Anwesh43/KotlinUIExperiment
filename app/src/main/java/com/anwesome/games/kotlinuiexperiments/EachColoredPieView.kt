182e11f8e3101e0dfe2246d6b9b711183ebf7ccd
Anwesh43 <anweshthecool0@gmail.com>
2017-11-27 16:11:52 +0530

    adding EachColoredPie

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/EachColoredPieView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/EachColoredPieView.kt
@@ -3,9 +3,12 @@ package com.anwesome.games.kotlinuiexperiments
 /**
  * Created by anweshmishra on 27/11/17.
  */
+
 import android.view.*
 import android.content.*
 import android.graphics.*
+
+val pieAvailableColors:Array<String> = arrayOf("#f44336","#FFC107","#1565C0","#880E4F","#FF5722","#00E676","#3F51B5","#673AB7")
 
 class EachColoredPieView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -19,5 +22,13 @@ class EachColoredPieView(ctx:Context):View(ctx) {
             }
         }
         return true
+    }
+    data class EachColoredPie(var i:Int,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            canvas.save()
+            paint.color = Color.parseColor(pieAvailableColors[i])
+            canvas.drawArc(RectF(-r,-r,r,r),90f*i,90*scale,true,paint)
+            canvas.restore()
+        }
     }
 }
