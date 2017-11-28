5d2c0c0eb68518e25294e21662d47c00f5b76ed8
Anwesh43 <anweshthecool0@gmail.com>
2017-11-28 05:57:53 +0530

    adding HorizontalBlockMoverView

---
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/HorizontalBlockMoverView.kt
@@ -0,0 +1,23 @@
+package com.anwesome.games.kotlinuiexperiments
+
+/**
+ * Created by anweshmishra on 28/11/17.
+ */
+import android.view.*
+import android.content.*
+import android.graphics.*
+
+class HorizontalBlockMoverView(ctx:Context,var n:Int = 4):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
