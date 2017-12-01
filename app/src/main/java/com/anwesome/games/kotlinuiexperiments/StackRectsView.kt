24a369a74eec0d514da7bd3cff131b8158607205
Anwesh43 <anweshthecool0@gmail.com>
2017-12-01 22:19:35 +0530

    adding static create method

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/StackRectsView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/StackRectsView.kt
@@ -12,13 +12,15 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 class StackRectsView(ctx:Context,var n:Int = 5):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = StackRectRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        canvas.drawColor(Color.parseColor("#212121"))
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
