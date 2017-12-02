9a5375a860a575d985f2253ffdc85280757b5e23
Anwesh43 <anweshthecool0@gmail.com>
2017-12-02 11:35:23 +0530

    adding Renderer in View

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossPieView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CrossPieView.kt
@@ -9,13 +9,14 @@ import android.graphics.*
 
 class CrossPieView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = CrossPieController(view = this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
