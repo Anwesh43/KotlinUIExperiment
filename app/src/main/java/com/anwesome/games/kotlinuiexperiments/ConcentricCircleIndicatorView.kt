49dbaee01f22ccfa773e244b44659e70889769c9
Anwesh43 <anweshthecool0@gmail.com>
2017-12-03 11:40:53 +0530

    adding ConcentricCircleContainer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ConcentricCircleIndicatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ConcentricCircleIndicatorView.kt
@@ -6,6 +6,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.graphics.*
 import android.content.*
 import android.view.*
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class ConcentricCircleIndicatorView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -23,9 +24,28 @@ class ConcentricCircleIndicatorView(ctx:Context):View(ctx) {
     data class ConcentricCircle(var r:Float) {
         fun draw(canvas:Canvas,paint:Paint) {
             paint.style = Paint.Style.STROKE
+            paint.strokeWidth = r/12
             canvas.drawArc(RectF(-r,-r,r,r),0f,360f,false,paint)
         }
         fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
+    }
+    data class ConcentricCircleContainer(var w:Float,var h:Float) {
+        var circles:ConcurrentLinkedQueue<ConcentricCircle> = ConcurrentLinkedQueue()
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            paint.color = Color.parseColor("#ff9800")
+            circles.forEach { circle ->
+                circle.draw(canvas,paint)
+            }
+            canvas.restore()
+        }
+        fun update(stopcb:(Float,Int)->Unit) {
 
         }
         fun startUpdating(startcb:()->Unit) {
