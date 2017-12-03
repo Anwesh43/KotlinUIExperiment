a7d3f90b027fa9ff1c64c836845563d962aa8621
Anwesh43 <anweshthecool0@gmail.com>
2017-12-03 11:50:28 +0530

    adding ConcentricCircleState

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ConcentricCircleIndicatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ConcentricCircleIndicatorView.kt
@@ -52,4 +52,19 @@ class ConcentricCircleIndicatorView(ctx:Context):View(ctx) {
 
         }
     }
+    data class ConcentricCircleState(var scale:Float = 0f,var dir:Float=0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale-prevScale) > 1) {
+                scale = prevScale+dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            dir = 1f-2*scale
+            startcb()
+        }
+    }
 }
