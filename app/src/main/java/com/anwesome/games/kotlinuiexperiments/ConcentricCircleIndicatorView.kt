e1592cac7824b4acf4af82a0fd0b7821b44b9016
Anwesh43 <anweshthecool0@gmail.com>
2017-12-03 12:52:24 +0530

    using renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/ConcentricCircleIndicatorView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/ConcentricCircleIndicatorView.kt
@@ -10,13 +10,14 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 class ConcentricCircleIndicatorView(ctx:Context,var n:Int=5):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = ConcentricCircleIndicatorRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
