54dc28e0d733ed4b5e5ec67302c2d726c2a1dad4
Anwesh43 <anweshthecool0@gmail.com>
2017-12-05 00:26:16 +0530

    adding RotateOverLine

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateOverLineView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateOverLineView.kt
@@ -20,4 +20,19 @@ class RotateOverLineView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class RotateOverLine(var x:Float,var y:Float,var size:Float,var deg:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.drawCircle(0f,0f,size/10,paint)
+            canvas.save()
+            canvas.rotate(deg)
+            canvas.drawLine(0f,0f,size,paint)
+            canvas.restore()
+            canvas.restore()
+        }
+        fun update() {
+            deg += 15f
+        }
+    }
 }
