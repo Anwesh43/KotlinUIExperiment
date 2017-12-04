15c971f9495da6f70b049e7f9784144ad4ce054e
Anwesh43 <anweshthecool0@gmail.com>
2017-12-05 00:40:27 +0530

    adding RotateOverLineContainer

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateOverLineView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/RotateOverLineView.kt
@@ -6,6 +6,7 @@ package com.anwesome.games.kotlinuiexperiments
 import android.view.*
 import android.content.*
 import android.graphics.*
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class RotateOverLineView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -20,19 +21,46 @@ class RotateOverLineView(ctx:Context):View(ctx) {
         }
         return true
     }
-    data class RotateOverLine(var x:Float,var y:Float,var size:Float,var deg:Float) {
+    data class RotateOverLine(var x:Float,var y:Float,var size:Float,var deg:Float=0f) {
         fun draw(canvas:Canvas,paint:Paint) {
             canvas.save()
             canvas.translate(x,y)
             canvas.drawCircle(0f,0f,size/10,paint)
             canvas.save()
             canvas.rotate(deg)
-            canvas.drawLine(0f,0f,size,paint)
+            canvas.drawLine(0f,0f,size,0f,paint)
             canvas.restore()
             canvas.restore()
         }
         fun update() {
             deg += 15f
+            deg %= 360f
+        }
+    }
+    data class RotateOverLineContainer(var w:Float,var h:Float) {
+        var rotateOverLines:ConcurrentLinkedQueue<RotateOverLine> = ConcurrentLinkedQueue()
+        var curr:RotateOverLine?=null
+        fun addNewLine(x:Float,y:Float) {
+            if(rotateOverLines.size == 0) {
+                curr = RotateOverLine(x,y,Math.min(w,h)/9)
+            }
+            else {
+                val x = curr?.x?:0f + Math.cos((curr?.deg?:0f)*Math.PI/180).toFloat()
+                val y = curr?.y?:0f + Math.sin((curr?.deg?:0f)*Math.PI/180).toFloat()
+                val deg = (((curr?.deg?:0f)-180)+360f)%360
+                curr = RotateOverLine(x,y,deg)
+            }
+            rotateOverLines.add(curr)
+        }
+        fun draw(canvas:Canvas,paint:Paint) {
+            canvas.save()
+            rotateOverLines.forEach { it ->
+                it.draw(canvas,paint)
+            }
+            canvas.restore()
+        }
+        fun update() {
+            curr?.update()
         }
     }
 }
