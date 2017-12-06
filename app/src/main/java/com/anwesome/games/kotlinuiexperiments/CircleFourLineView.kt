f98e6ea237fc58bc46fabfd345acfafef6e1caf6
Anwesh43 <anweshthecool0@gmail.com>
2017-12-06 20:35:13 +0530

    adding CircleFourLine

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircleFourLineView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircleFourLineView.kt
@@ -20,4 +20,23 @@ class CircleFourLineView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class CircleFourLine(var x:Float,var y:Float,var r:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            paint.strokeWidth = r/12
+            canvas.save()
+            canvas.translate(x,y)
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(0f,0f,r,paint)
+            for(i in 0..3) {
+                canvas.save()
+                canvas.rotate(90f*i)
+                canvas.drawLine(r-(0.8f*r*scale),0f,r,0f,paint)
+                canvas.restore()
+            }
+            canvas.drawCircle(0f,0f,r/5,paint)
+            paint.style = Paint.Style.FILL
+            canvas.drawArc(RectF(-r/5,-r/5,r/5,r/5),0f,360f,true,paint)
+            canvas.restore()
+        }
+    }
 }
