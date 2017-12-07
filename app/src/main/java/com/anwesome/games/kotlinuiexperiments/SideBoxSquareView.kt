eebbe6b7ca74673e620d79a3a18b02b4f82bd38a
Anwesh43 <anweshthecool0@gmail.com>
2017-12-08 02:35:09 +0530

    adding SideBoxSquareState

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/SideBoxSquareView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/SideBoxSquareView.kt
@@ -28,11 +28,25 @@ class SideBoxSquareView(ctx:Context,var n:Int = 6):View(ctx) {
             canvas.drawRoundRect(RectF(-size/2,-size/2,size/2,size/2),size/10,size/10,paint)
             canvas.restore()
         }
-        fun update(stopcb:()->Unit) {
+        fun update(stopcb:(Float)->Unit) {
 
         }
         fun startUpdating(startcb:()->Unit) {
 
+        }
+    }
+    data class SideBoxSquareState(var scale:Float=0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale-prevScale)>1) {
+                scale = prevScale+dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            dir = 1f-2*scale
         }
     }
 }
