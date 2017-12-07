f7f43cb200ca73245f56f98e0a6ded00ee4fdbd3
Anwesh43 <anweshthecool0@gmail.com>
2017-12-07 08:24:52 +0530

    adding DirecArrowPie

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirecArrowPieView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/DirecArrowPieView.kt
@@ -7,6 +7,8 @@ import android.view.*
 import android.content.*
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
 import android.view.*
 
 class DirecArrowPieView(ctx:Context):View(ctx) {
@@ -22,4 +24,34 @@ class DirecArrowPieView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class DirecArrowPie(var w:Float,var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            paint.style = Paint.Style.STROKE
+            val r = Math.min(w,h)/5
+            canvas.drawArc(RectF(-r,-r,r,r),0f,360f*scale,false,paint)
+            canvas.restore()
+            paint.style = Paint.Style.FILL
+            val size = Math.min(w,h)/10
+            for(i in 0..1) {
+                canvas.save()
+                canvas.translate(w/2,h/5+h/2*i)
+                canvas.rotate(180f*scale)
+                for(j in 0..1) {
+                    canvas.save()
+                    canvas.scale(1f-2*j,1f)
+                    val path = Path()
+                    path.moveTo(0f, -size / 2)
+                    path.lineTo(size, 0f)
+                    path.lineTo(0f, size / 2)
+                    canvas.drawPath(path, paint)
+                    canvas.restore()
+                }
+                canvas.restore()
+
+            }
+        }
+    }
 }
+
