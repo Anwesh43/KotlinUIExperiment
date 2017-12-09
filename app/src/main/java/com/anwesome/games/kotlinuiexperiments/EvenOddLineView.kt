69fd3716e0e68a62bc3991302f5ce65ae07d9cd1
Anwesh43 <anweshthecool0@gmail.com>
2017-12-09 08:31:51 +0530

    adding EvenOddLine

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/EvenOddLineView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/EvenOddLineView.kt
@@ -20,4 +20,14 @@ class EvenOddLineView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class EvenOddLine(var i:Int,var x:Float,var py:Float,var h:Float,var y:Float=py) {
+        fun draw(canvas:Canvas,paint:Paint,scale:Float) {
+            val j = i%2
+            y = - h*(j+(1-2*j)*scale)
+            canvas.save()
+            canvas.translate(x,py)
+            canvas.drawLine(0f,0f,0f,y,paint)
+            canvas.restore()
+        }
+    }
 }
