cf15459af636bdc82e69766939538f057aa3b4c5
Anwesh43 <anweshthecool0@gmail.com>
2017-12-10 10:20:01 +0530

    adding renderer in view

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircleDotOverView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircleDotOverView.kt
@@ -10,12 +10,14 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 class CircleDotOverView(ctx:Context,var n:Int = 10):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = CircleDotRenderer(this)
     override fun onDraw(canvas:Canvas) {
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
