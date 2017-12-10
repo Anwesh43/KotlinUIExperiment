31491620606626598e12f4b89a6b6f91b2854941
Anwesh43 <anweshthecool0@gmail.com>
2017-12-10 09:10:28 +0530

    adding CircleDot

--- app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircleDotOverView.kt
+++ app/src/main/java/com/anwesome/games/kotlinuiexperiments/CircleDotOverView.kt
@@ -19,4 +19,25 @@ class CircleDotOverView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class CircleDot(var i:Int,var x:Float,var y:Float,var r:Float,var deg:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            val new_deg = deg*i
+            val update_deg = deg
+            canvas.save()
+            canvas.translate(x,y)
+            canvas.rotate(new_deg+update_deg)
+            paint.style = Paint.Style.FILL
+            canvas.drawCircle(0f,0f,r,paint)
+            canvas.restore()
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = r/7
+            canvas.drawArc(RectF(-r,-r,r,r),new_deg,update_deg,false,paint)
+        }
+        fun update(stopcb:(Int,Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
+        }
+    }
 }
